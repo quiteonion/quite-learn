9a1ee226854bcb96d97578bbb8bd4a41723c60d1
好喜欢安静啊 <3345973813@qq.com>
2024-01-11 21:30:18 +0800

    init

--- src/Day008/GuessNumber.java
+++ src/Day008/GuessNumber.java
@@ -9,7 +9,7 @@ public class GuessNumber {
         Scanner sc = new Scanner(System.in);
 
         int GuessNumber0 = r.nextInt(100) + 1;
-        int a = 5;
+        /*int a = 5;
         while (true) {
             System.out.print("请输入你认为正确的答案：");
             int number = sc.nextInt();
@@ -27,11 +27,24 @@ public class GuessNumber {
                     a -= 1;
                 }
             }else {
-                System.out.println("你已经失去了所有的机会，答案是" + GuessNumber0 + "，下次努力！");
-                break;
+
             }
-        }
-
-
+        }*/
+        for (int a = 5; a >= 1; a--) {
+            System.out.print("请输入你认为正确的答案：");
+            int number = sc.nextInt();
+                if (number == GuessNumber0) {
+                    System.out.println("恭喜你猜对了");
+                    break;
+                } else {
+                    if (number > GuessNumber0) {
+                        System.out.print("你输入的数字太大了！");
+                    } else {
+                        System.out.print("你输入的数字太小了！");
+                    }
+                    System.out.println("你还剩下" + (a-1) + "次机会");
+                }
+            }
+        System.out.println("你已经失去了所有的机会，答案是" + GuessNumber0 + "，下次努力！");
     }
 }
