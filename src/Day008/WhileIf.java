5431fda1c89d31f86285c9b446cd4530aaa19595
好喜欢安静啊 <3345973813@qq.com>
2024-01-11 21:06:30 +0800

    init

---
+++ src/Day008/WhileIf.java
@@ -0,0 +1,13 @@
+package Day008;
+
+public class WhileIf {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100){
+            if (i %2 == 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
