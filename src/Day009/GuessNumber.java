ef69bec91d43fd43b0bf0c1ba0de2f25715be3c8
好喜欢安静啊 <3345973813@qq.com>
2024-01-12 21:13:10 +0800

    init

--- src/Day009/GuessNumber.java
+++ src/Day009/GuessNumber.java
@@ -1,9 +1,23 @@
 package Day009;
 
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessNumber {
     public static void main(String[] args) {
-        while(){
-
+        Random r = new Random();
+        int b =r.nextInt(100)+1;
+        while(true){
+            Scanner sc =new Scanner(System.in);
+            int a = sc.nextInt();
+            if (a == b ){
+                System.out.println("yes");
+            } else if (a<b) {
+                System.out.println("small");
+            }else {
+                System.out.println("big");
+            }
         }
     }
 }
