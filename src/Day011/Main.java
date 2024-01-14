b4de3f7f3d2d81b742019937e0d962a6d129daae
好喜欢安静啊 <3345973813@qq.com>
2024-01-14 17:13:12 +0800

    init

---
+++ src/Day011/Main.java
@@ -0,0 +1,13 @@
+package Day011;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String i = sc.next();
+            System.out.println("那怎么办？");
+        }
+    }
+}
