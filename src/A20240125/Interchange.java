e02e193a4f7fc900401295cb79fcf37f8fe8bc84
好喜欢安静啊 <3345973813@qq.com>
2024-01-25 14:48:31 +0800

    Interchange

---
+++ src/A20240125/Interchange.java
@@ -0,0 +1,16 @@
+package A20240125;
+
+public class Interchange {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+
+        int z;
+        z = x;
+        x = y;
+        y = z;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
