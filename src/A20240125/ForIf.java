9746551aa0b2caebf1f94cd6c70209a3cb0b73a3
好喜欢安静啊 <3345973813@qq.com>
2024-01-25 16:33:41 +0800

    add ForIf

---
+++ src/A20240125/ForIf.java
@@ -0,0 +1,11 @@
+package A20240125;
+
+public class ForIf {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            if (i != 4) {
+                System.out.println(i);
+            }
+        }
+    }
+}
