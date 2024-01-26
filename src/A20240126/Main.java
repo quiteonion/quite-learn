c2f31eb44b3bdf35baf2a2633e89bd32b14f2a58
好喜欢安静啊 <3345973813@qq.com>
2024-01-26 17:05:09 +0800

    容器 i 仍然是整型，所以输出结果是 0

--- src/A20240126/Main.java
+++ src/A20240126/Main.java
@@ -1,4 +1,10 @@
 package A20240126;
 
 public class Main {
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 3;
+        j*=i++;
+        System.out.println(i);
+    }
 }
