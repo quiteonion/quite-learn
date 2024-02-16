14cbd43eb8afcdc374f521c00850ba4679e225fb
好喜欢安静啊 <3345973813@qq.com>
2024-02-16 15:04:39 +0800

    数组中隐藏的电话号码

---
+++ src/Job/Telephone.java
@@ -0,0 +1,13 @@
+package Job;
+
+public class Telephone {
+    public static void main(String[] args) {
+        int[] arr = {1,4,5,2,3,7,6,8,9,3};
+        int[] arr1 = {0,5,5,2,8,6,6,0,9,5,3};
+
+        for (int i = 0; i < arr1.length; i++) {
+            int num = arr1[i];
+            System.out.print(arr[num]);
+        }
+    }
+}
