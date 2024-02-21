20a508684d6f5a81093c26ffce41d2af414673c9
好喜欢安静啊 <3345973813@qq.com>
2024-02-21 15:19:38 +0800

    LinearSearch-step-0

--- src/Job20240216Over/LinearSearch.java
+++ src/Job20240216Over/LinearSearch.java
@@ -4,20 +4,7 @@ import java.util.Scanner;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int[] arr = {1,3,5,8,9,0,7,6,2,4};
-        boolean flag = true;
-        System.out.print("请输入您要查找的数字：");
-        int number = sc.nextInt();
-        for (int i = 0; i < arr.length; i++) {
-            if (number == arr[i]){
-                System.out.println("您查找的数字存在于数组中，下标为" + i);
-                flag = false;
-                break;
-            }
-        }
-        if (flag){
-            System.out.println("您查找的数字不存在");
-        }
+        //已知一个数组，要求输入要查找的数字，如果存在Z则显示下标并输出
     }
 }
