29cf279ae66a64aa3d4b8d51494236df3a832fd4
好喜欢安静啊 <3345973813@qq.com>
2024-02-21 17:12:35 +0800

    BubbleSort-step-1

--- src/Job20240216Over/BubbleSort.java
+++ src/Job20240216Over/BubbleSort.java
@@ -1,12 +1,24 @@
 package Job20240216Over;
 
-import java.util.Scanner;
+
 
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 8, 9, 0, 7, 6, 2, 4};
         //存在一个数组，数组内容混乱，要求排序整理
-
-
+        //使用冒泡排序法
+        /**
+         * 冒泡排序法：
+         * 由前到后逐一对比逐一排序
+         */
+        //先让  arr[0] 与arr[1] 对比
+        int i = arr[0] > arr[1] ? 1 : 0;
+        //如果arr[0] 比arr[1]大就交换位置
+        if (arr[0]>arr[1]){
+            int o = arr[0];
+            arr[0] = arr[1];
+            arr[1] = o;
+        }
+        //以此类推，直至结束
     }
 }
