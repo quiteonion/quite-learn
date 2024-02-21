1115a004ddf1b70e57c86b5c41995b82625ff8f9
好喜欢安静啊 <3345973813@qq.com>
2024-02-21 14:49:33 +0800

    Telephone-step-0

--- src/Job20240216Over/Telephone.java
+++ src/Job20240216Over/Telephone.java
@@ -4,10 +4,11 @@ public class Telephone {
     public static void main(String[] args) {
         int[] arr = {1,4,5,2,3,7,6,8,9,3};
         int[] arr1 = {0,5,5,2,8,6,6,0,9,5,3};
+        /**
+         * 已知条件
+         * 1、两个数组
+         * 2、从数组内提取出电话号码
+         */
 
-        for (int i = 0; i < arr1.length; i++) {
-            int num = arr1[i];
-            System.out.print(arr[num]);
-        }
     }
 }
