62a084087aa73c58c516cf4f9830a72f27860229
好喜欢安静啊 <3345973813@qq.com>
2024-02-21 14:31:29 +0800

    Table99-step-1

--- src/Job20240216Over/Table99.java
+++ src/Job20240216Over/Table99.java
@@ -2,6 +2,21 @@ package Job20240216Over;
 
 public class Table99 {
     public static void main(String[] args) {
-       /*关于如何书写一个九九乘法表*/
+        /*关于如何书写一个九九乘法表*/
+        //九九乘法表结构为
+        /**
+         * 1*1
+         * 2*1  2*2
+         * 3*1  3*2  3*3
+         * .。。。。。
+         * 。。。
+         * 。。
+         * 。
+         */
+        //自然可以选择使用”直接显示法“
+        System.out.println("1*1=2");
+        System.out.println("1*2=2  2*2=4");
+        System.out.println("1*3=3  2*3=6  3*3=9");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 }
