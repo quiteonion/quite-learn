2e95fa3fee3ea709324b29dab404bc8278739992
好喜欢安静啊 <3345973813@qq.com>
2024-02-21 18:04:20 +0800

    BinarySearch-step-4

--- src/Job20240216Over/BinarySearch.java
+++ src/Job20240216Over/BinarySearch.java
@@ -14,18 +14,26 @@ public class BinarySearch {
         int number= scanner.nextInt();
 
         //所谓二分法就是 “切一半” 、 把数字应该存在的部分切下来，然后对比寻找
-        //那么就会有三把刀
+        //那么就会有两把把刀
         int zuo = 0;
         int you = arr.length;
-        int zhong = arr.length/2;
-        //三把刀悬着，等到判断出来是要哪个部分之后就切掉不需要的那一半
+        //两把刀悬着，等到判断出来是要哪个部分之后就切掉不需要的那一半
 
         //那么就需要循环，直到找到要的部分为止
         //循环两类for 和 while 我们不清楚什么时候会找到他，但是我们先出发，使用while（true）{我们不知道结束条件是什么}
         while(true){
             //在循环中就是循环我们要的“切蛋糕”环节了
             //先判断我们要的，比如说草莓蛋糕上的草莓，在那一边
-            if ()
+            if (number == (zuo+you)/2){
+                System.out.println("这个数字存在");
+                break;
+            } else if (number > (zuo+you)/2){
+                zuo = (zuo+you)/2;
+            }else {
+                you = (zuo+you)/2;
+            }
         }
+
+
     }
 }
