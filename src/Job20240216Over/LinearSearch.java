8f714b0937f8670385f93691abacf5bd323b3567
好喜欢安静啊 <3345973813@qq.com>
2024-02-21 15:20:46 +0800

    LinearSearch-step-1

--- src/Job20240216Over/LinearSearch.java
+++ src/Job20240216Over/LinearSearch.java
@@ -6,5 +6,10 @@ public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {1,3,5,8,9,0,7,6,2,4};
         //已知一个数组，要求输入要查找的数字，如果存在Z则显示下标并输出
+
+        //键盘录入必须要
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入您要查找的元素");
+        int Number = scanner.nextInt();
     }
 }
