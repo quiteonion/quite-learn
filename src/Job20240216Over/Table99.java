180e314be77e524f5fec22860599395deb923c80
好喜欢安静啊 <3345973813@qq.com>
2024-02-21 14:27:51 +0800

    Table99-step-0

--- src/Job20240216Over/Table99.java
+++ src/Job20240216Over/Table99.java
@@ -2,14 +2,6 @@ package Job20240216Over;
 
 public class Table99 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 9; i++) {
-            /*外循环建立行*/
-            for (int j = 1; j <= i; j++) {
-                /*内循环成立列*/
-                System.out.print(i + " * " + j + " = " + i * j + "   ");
-            }
-            System.out.println();
-            /*换行*/
-        }
+       /*关于如何书写一个九九乘法表*/
     }
 }
