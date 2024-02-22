99e4df96dfd42e0e7200acf4597de5992f2612d5
好喜欢安静啊 <3345973813@qq.com>
2024-02-22 17:09:03 +0800

    削减

--- src/A20240222/DogTest.java
+++ src/A20240222/DogTest.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 public class DogTest {
     public static void main(String[] args) {
         Dog dog = new Dog("小七", 001 , "黑白相间的灰");
-        Dog d = new Dog();
-        d.setName("老八");
-        d.setId(002);
-        d.setColor("五彩斑斓的黑");
+        Dog d = new Dog("老八", 002 , "五彩斑斓的黑");
+
         Dog[] arr = new Dog[]{dog,d};
 
-        System.out.print(dog.getName()+"的编号是"+dog.getId()+",毛色是"+dog.getColor()+",");dog.show();
+        System.out.print(dog.name+"的编号是"+dog.id+",毛色是"+dog.color+",");dog.show();
         System.out.println();
-        System.out.print(d.getName()+"的编号是"+d.getId()+",毛色是"+d.getColor()+",");d.show();
+        System.out.print(d.name+"的编号是"+d.id+",毛色是"+d.color+",");d.show();
     }
 }
