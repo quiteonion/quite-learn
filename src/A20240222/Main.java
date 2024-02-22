80ef8cb07ca7546dbcd1a4d24eec1ba5bf26b477
好喜欢安静啊 <3345973813@qq.com>
2024-02-22 10:31:44 +0800

    简便循环

---
+++ src/A20240222/Main.java
@@ -0,0 +1,10 @@
+package A20240222;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,7,12,4,6,65,9,78};
+        System.out.println(Arrays.toString(arr));
+    }
+}
