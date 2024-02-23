d68f2120a0a99c294f6dab9ffdab9bf374414338
好喜欢安静啊 <3345973813@qq.com>
2024-02-23 12:08:51 +0800

    狗哥进化
    set 的影响----狗哥与 set 的关系

--- src/A20240223/Dog.java
+++ src/A20240223/Dog.java
@@ -1,11 +1,63 @@
 package A20240223;
 
 public class Dog {
-    /*private*/ String name;
-    /*private*/ int id;
-    /*private*/ String color;
+    private String name;
+    private int id;
+    private String color;
 
     public void Eat(){
         System.out.println("狗哥在吃东西");
     }
+/*
+    */
+/**
+     * 获取
+     * @return name
+     *//*
+
+    public String getName() {
+        return name;
+    }
+*/
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+/*    public int getId() {
+        return id;
+    }
+
+    *//**
+     * 设置
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return color
+     */
+/*    public String getColor() {
+        return color;
+    }
+
+    *//**
+     * 设置
+     * @param color
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }
--- src/A20240223/DogTest.java
+++ src/A20240223/DogTest.java
@@ -3,9 +3,12 @@ package A20240223;
 public class DogTest {
     public static void main(String[] args) {
         Dog dog = new Dog();
-        dog.name = "黑子";
+        /*dog.name = "黑子";
         dog.id = 1;
         dog.color = "bule";
-        System.out.print(dog.name+"的编号是"+dog.id+"毛发颜色是"+dog.color);dog.Eat();
+        System.out.print(dog.name+"的编号是"+dog.id+"毛发颜色是"+dog.color);dog.Eat();*/
+        dog.setName("黑子");
+        dog.setId(1);
+        dog.setColor("bule");
     }
 }
