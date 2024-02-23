ecd58ff3cac1168de55eff723823adeeedb11d22
好喜欢安静啊 <3345973813@qq.com>
2024-02-23 12:05:04 +0800

    狗哥初始化

---
+++ src/A20240223/Dog.java
@@ -0,0 +1,11 @@
+package A20240223;
+
+public class Dog {
+    /*private*/ String name;
+    /*private*/ int id;
+    /*private*/ String color;
+
+    public void Eat(){
+        System.out.println("狗哥在吃东西");
+    }
+}
---
+++ src/A20240223/DogTest.java
@@ -0,0 +1,11 @@
+package A20240223;
+
+public class DogTest {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.name = "黑子";
+        dog.id = 1;
+        dog.color = "bule";
+        System.out.print(dog.name+"的编号是"+dog.id+"毛发颜色是"+dog.color);dog.Eat();
+    }
+}
