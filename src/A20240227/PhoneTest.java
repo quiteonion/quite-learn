93ad5ba86a9f4031134f3cd329c69b50d262fac2
好喜欢安静啊 <3345973813@qq.com>
2024-02-28 13:05:57 +0800

    手机数组  面向对象

---
+++ src/A20240227/PhoneTest.java
@@ -0,0 +1,32 @@
+package A20240227;
+
+import java.util.Scanner;
+
+public class PhoneTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Phone[] arr = new Phone[3];
+        for (int i = 0; i < arr.length; i++) {
+            Phone p = new Phone();
+            System.out.print("手机的品牌：");
+            String brand = sc.next();
+            p.setBrand(brand);
+            System.out.print("手机的价格：");
+            double price = sc.nextDouble();
+            p.setPrice(price);
+            System.out.print("手机的颜色：");
+            String color = sc.next();
+            p.setColor(color);
+            arr[i] = p;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i].getPrice());
+        }
+        double number = 0;
+        for (int i = 0; i < arr.length; i++) {
+            number = number+arr[i].getPrice();
+        }
+
+        System.out.println("平均价格为"+number/arr.length);
+    }
+}
