847a120948aeb2d2d4f68a12e76da07971dbd694
好喜欢安静啊 <3345973813@qq.com>
2024-03-03 09:51:20 +0800

    Student-step-4
    如果要单独更改一个元素，就要使用到空参构造和set方法

--- src/A20240303/Student.java
+++ src/A20240303/Student.java
@@ -13,6 +13,8 @@ public class Student {
         this.id = id;
     }
 
+    public Student() {
+    }
 
 
     public String getName() {
@@ -30,6 +32,38 @@ public class Student {
 
     public int getId() {
         return id;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * 设置
+     * @param gender
+     */
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
     }
 
 }
--- src/A20240303/StudentTest.java
+++ src/A20240303/StudentTest.java
@@ -5,6 +5,12 @@ import A20240303.Student;
 public class StudentTest {
     public static void main(String[] args) {
         Student student = new Student("小黑",18,'男',01);
+        Student s = new Student();
+        s.setName("hhh");
+        s.setId(02);
+        s.setAge(19);
+        s.setGender('女');
         System.out.println("学生姓名： "+student.getName()+"年龄： "+student.getAge()+"id： "+student.getId()+"性别： "+student.getGender());
+        System.out.println("学生姓名： "+s.getName()+"年龄： "+s.getAge()+"id： "+s.getId()+"性别： "+s.getGender());
     }
 }
