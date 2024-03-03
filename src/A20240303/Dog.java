fa70c4e2617bf5de99917e8b8488f33906b73a99
好喜欢安静啊 <3345973813@qq.com>
2024-03-03 15:36:25 +0800

    杂项

---
+++ src/A20240303/Dog.java
@@ -0,0 +1,48 @@
+package A20240303;
+
+public class Dog {
+    private String name;
+    private int id;
+
+    public Dog() {
+    }
+
+    public Dog(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
---
+++ src/A20240303/DogTest.java
@@ -0,0 +1,9 @@
+package A20240303;
+
+public class DogTest {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Student student = new Student();
+        Dog[] arr = new Dog[3];
+    }
+}
