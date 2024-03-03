dc72fdae2e8a83ddc848adb73008affdbfa38b65
好喜欢安静啊 <3345973813@qq.com>
2024-03-03 09:55:50 +0800

    Student-step-6
    学生对象就以这种形式成就,但是纵使是可以使用两种方法创建学生对象，也不能够混用

--- src/A20240303/StudentTest.java
+++ src/A20240303/StudentTest.java
@@ -5,12 +5,19 @@ import A20240303.Student;
 public class StudentTest {
     public static void main(String[] args) {
         Student student = new Student("小黑",18,'男',01);
+
         Student s = new Student();
         s.setName("hhh");
         s.setId(02);
         s.setAge(19);
         s.setGender('女');
+
+        /*Student s1 = new Student("小红",17);
+        s1.setGender('女');
+        s1.setId(03);*/
+
         System.out.println("学生姓名： "+student.getName()+"年龄： "+student.getAge()+"id： "+student.getId()+"性别： "+student.getGender());
         System.out.println("学生姓名： "+s.getName()+"年龄： "+s.getAge()+"id： "+s.getId()+"性别： "+s.getGender());
+
     }
 }
