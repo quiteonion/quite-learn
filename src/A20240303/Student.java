95867fdff0776ae979c66685fa3f0106114ce857
好喜欢安静啊 <3345973813@qq.com>
2024-03-03 09:48:44 +0800

    Student-step-4
    导入的对象要将他提取出来，使用到 get

--- src/A20240303/Student.java
+++ src/A20240303/Student.java
@@ -1,9 +1,35 @@
 package A20240303;
 
 public class Student {
-    String name;
-    int age;
-    char gender;
-    int id;
+    private String name;
+    private int age;
+    private char gender;
+    private int id;
+
+    public Student(String name, int age, char gender, int id) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.id = id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getId() {
+        return id;
+    }
 
 }
--- src/A20240303/StudentTest.java
+++ src/A20240303/StudentTest.java
@@ -4,11 +4,7 @@ import A20240303.Student;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.name = "小黑";
-        student.age = 18;
-        student.gender = '男';
-        student.id  = 01;
-        System.out.println("学生姓名： "+student.name+"年龄： "+student.age+"id： "+student.id+"性别： "+student.gender);
+        Student student = new Student("小黑",18,'男',01);
+        System.out.println("学生姓名： "+student.getName()+"年龄： "+student.getAge()+"id： "+student.getId()+"性别： "+student.getGender());
     }
 }
