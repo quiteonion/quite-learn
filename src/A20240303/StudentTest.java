27810a6c37466259acee45672caab1cb76c9c735
好喜欢安静啊 <3345973813@qq.com>
2024-03-03 09:40:50 +0800

    Student-step-3
    创建一个学生对象

--- src/A20240303/StudentTest.java
+++ src/A20240303/StudentTest.java
@@ -1,7 +1,14 @@
 package A20240303;
 
+import A20240303.Student;
+
 public class StudentTest {
     public static void main(String[] args) {
-
+        Student student = new Student();
+        student.name = "小黑";
+        student.age = 18;
+        student.gender = '男';
+        student.id  = 01;
+        System.out.println("学生姓名： "+student.name+"年龄： "+student.age+"id： "+student.id+"性别： "+student.gender);
     }
 }
