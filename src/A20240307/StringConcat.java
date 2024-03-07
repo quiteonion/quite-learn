c59ca95694f72939cda4939e9356925275266bf3
好喜欢安静啊 <3345973813@qq.com>
2024-03-07 13:40:40 +0800

    字符串拼接-3
    构建一个方法，拼接字符串并输出

--- src/A20240307/StringConcat.java
+++ src/A20240307/StringConcat.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class StringConcat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String separate;
         while (true) {
             System.out.println("请输入你所想要用作分隔符的字符（只能是一个！）");
-            String separate = sc.next();
+            separate = sc.next();
             if (separate.length() == 1) {
                 break;
             }
@@ -17,8 +17,17 @@ public class StringConcat {
 
         System.out.println("请输入一串字符串");
         String concat = sc.next();
+        String m = con(separate, concat, "你好", "世界", "!");
+        System.out.println(m);
 
     }
 
-
+    public static String con(String... concat) {
+        String m = "";
+        for (int i = 1; i < concat.length - 1; i++) {
+            m = m + concat[i] + concat[0];
+        }
+        m = m + concat[concat.length];
+        return m;
+    }
 }
