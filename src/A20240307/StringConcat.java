9d20489b36ab9d4594e1920335a7fa0d7b0f3516
好喜欢安静啊 <3345973813@qq.com>
2024-03-07 13:25:53 +0800

    字符串拼接-1
    进行一个判断，内容为：如果用户输入的不是一个字符就进行重新输入

--- src/A20240307/StringConcat.java
+++ src/A20240307/StringConcat.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 public class StringConcat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("请输入你所想要用作分隔符的字符（只能是一个！）");
 
-
+        while (true) {
+            System.out.println("请输入你所想要用作分隔符的字符（只能是一个！）");
+            String separate = sc.next();
+            if (separate.length() == 1){
+                break;
+            }
+            System.out.println("您的输入格式有误，请重新输入！");
+        }
 
     }
 }
