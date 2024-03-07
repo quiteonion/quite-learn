9f84b390e1f97688e73a56ecc6d26169924a6ca0
好喜欢安静啊 <3345973813@qq.com>
2024-03-07 13:24:55 +0800

    字符串拼接-0
    输入要用作拼接的字符

---
+++ src/A20240307/StringConcat.java
@@ -0,0 +1,13 @@
+package A20240307;
+
+import java.util.Scanner;
+
+public class StringConcat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入你所想要用作分隔符的字符（只能是一个！）");
+
+
+
+    }
+}
