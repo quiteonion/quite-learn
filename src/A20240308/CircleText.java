06573d6ed4c5eeef67074e4e83f1af51ac7ce2b9
好喜欢安静啊 <3345973813@qq.com>
2024-03-08 21:11:50 +0800

    this 究竟是什么？？
    双重 new 代码版

--- src/A20240308/CircleText.java
+++ src/A20240308/CircleText.java
@@ -3,7 +3,11 @@ package A20240308;
 public class CircleText {
     public static void main(String[] args) {
         Circle c = new Circle(3);
-        System.out.println(c);
+        System.out.println("circle = "+c);
 
+        System.out.println("==========================");
+
+        Circle c1 = new Circle(3);
+        System.out.println("circle1 = "+c1);
     }
 }
