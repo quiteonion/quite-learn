31f163ca4305578d61d7989dafd11a6398ffaee4
好喜欢安静啊 <3345973813@qq.com>
2024-03-10 11:22:40 +0800

    单一实例
    添加 get 和 set 使其可以被调用

--- src/A20240308/Student.java
+++ src/A20240308/Student.java
@@ -9,6 +9,28 @@ public class Student {
     private Student() {
 
     }
+
+    public Student(Student s, String name, int age) {
+        this.s = s;
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return s
+     */
+    public static Student getS() {
+        return s;
+    }
+
+    /**
+     * 设置
+     * @param s
+     */
+    public static void setS(Student s) {
+        Student.s = s;
+    }
    /* public static Student getS() {
        Student s = new Student();
         return s;
@@ -51,4 +73,7 @@ public class Student {
         this.age = age;
     }
 
+    public String toString() {
+        return "Student{s = " + s + ", name = " + name + ", age = " + age + "}";
+    }
 }
--- src/A20240308/StudentText.java
+++ src/A20240308/StudentText.java
@@ -1,10 +1,10 @@
 package A20240308;
 public class StudentText {
     public static void main(String[] args) {
-        Student s1 = Student.s;
+        Student s1 = Student.getS();
         System.out.println(s1);
 
-        Student s2 = Student.s;
+        Student s2 = Student.getS();
         System.out.println(s2);
         /**
          * 以下是错误的代码展示
