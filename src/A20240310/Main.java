0666d2a85612dbe2476cee692177f94ff4743f06
好喜欢安静啊 <3345973813@qq.com>
2024-03-10 22:19:00 +0800

    单一实例（练习）-7
    使用 behavior 来构建单一实例

--- src/A20240310/Main.java
+++ src/A20240310/Main.java
@@ -10,7 +10,9 @@ public class Main {
     }
 
     public static Main getInstance() {
-
+        if (instance == null){
+            instance = new Main();
+        }
         return instance;
     }
 
