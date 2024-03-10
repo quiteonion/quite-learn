d481a2f8a2717bc13c1c064c081d399a82a17f98
好喜欢安静啊 <3345973813@qq.com>
2024-03-10 22:16:15 +0800

    单一实例（练习）-6
    检验发现完成单一实例

--- src/A20240310/MainTest.java
+++ src/A20240310/MainTest.java
@@ -4,7 +4,9 @@ import A20240104.Student;
 
 public class MainTest {
     public static void main(String[] args) {
-        Main m = Main.getInstance();
-        System.out.println(m);
+        Main m1= Main.getInstance();
+        System.out.println(m1);
+        Main m2= Main.getInstance();
+        System.out.println(m2);
     }
 }
