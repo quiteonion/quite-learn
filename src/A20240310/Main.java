cfa9b8c9baa79d323816b0f0c761749cb8d3605f
好喜欢安静啊 <3345973813@qq.com>
2024-03-10 14:32:58 +0800

    单一实例（练习）-2
    其次需要在对象中 先new出来，再提供使用，这样子就可以实现单一实例

--- src/A20240310/Main.java
+++ src/A20240310/Main.java
@@ -3,6 +3,9 @@ package A20240310;
 public class Main {
     private String name;
 
+    Main instance = new Main();
+
+
     private Main(){}
 
 
--- src/A20240310/MainTest.java
+++ src/A20240310/MainTest.java
@@ -4,6 +4,6 @@ import A20240104.Student;
 
 public class MainTest {
     public static void main(String[] args) {
-        Main instance = new Main();
+        Main instance = Main;
     }
 }
