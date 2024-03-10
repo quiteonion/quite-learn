655bfcbf94fe51cefbe0dd8ec99b9f5877f375d6
好喜欢安静啊 <3345973813@qq.com>
2024-03-10 14:34:57 +0800

    单一实例（练习）-3
    接下来就要想办法让 Test 调用到，而外界调用内部要使用方法

--- src/A20240310/Main.java
+++ src/A20240310/Main.java
@@ -3,10 +3,13 @@ package A20240310;
 public class Main {
     private String name;
 
-    Main instance = new Main();
+     Main instance = new Main();
 
 
     private Main(){}
+    public static Main getInstance(){
+        return instance;
+    }
 
 
 }
