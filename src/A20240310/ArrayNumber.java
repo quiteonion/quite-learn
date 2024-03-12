f41d673db820e2cb495472b4d2311f03a10e0585
好喜欢安静啊 <3345973813@qq.com>
2024-03-12 20:33:40 +0800

    Arrays.toString 的使用

---
+++ src/A20240310/ArrayNumber.java
@@ -0,0 +1,12 @@
+package A20240310;
+
+import java.util.Arrays;
+
+public class ArrayNumber {
+    public static void main(String[] args) {
+
+        int[] arr ={1,2,3,4,5,6,7,8,9,0};
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
