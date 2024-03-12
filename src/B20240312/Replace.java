475e27a47ff3cf74d8bd51cf57f70494b3696863
好喜欢安静啊 <3345973813@qq.com>
2024-03-12 21:00:14 +0800

    Replace 方法 -- 更改字符串的内容

---
+++ src/B20240312/Replace.java
@@ -0,0 +1,11 @@
+package B20240312;
+
+public class Replace {
+    public static void main(String[] args) {
+        String a = "quite";
+
+        String b = a.replace("q","a");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
