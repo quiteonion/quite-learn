8af4560f6e73e9fcf476fdd3412e566937236beb
好喜欢安静啊 <3345973813@qq.com>
2024-03-12 21:01:59 +0800

    Substring 方法 -- 截取字符串内容

---
+++ src/B20240312/SubString.java
@@ -0,0 +1,14 @@
+package B20240312;
+
+public class SubString {
+    public static void main(String[] args) {
+        String a = "quite";
+
+        String b = a.substring(0, 2);
+        String c = a.substring(1);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
