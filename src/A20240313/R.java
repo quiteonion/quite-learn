3e688fdaaee448778cac390e9daa6bf7359aca17
好喜欢安静啊 <3345973813@qq.com>
2024-03-13 20:52:31 +0800

    R 的练习题 无限循环的月读

---
+++ src/A20240313/R.java
@@ -0,0 +1,8 @@
+package A20240313;
+
+public class R {
+    public void m(){
+        System.out.println("=====");
+        m();
+    }
+}
---
+++ src/A20240313/RTest.java
@@ -0,0 +1,8 @@
+package A20240313;
+
+public class RTest {
+    public static void main(String[] args) {
+        R r = new R();
+        r.m();
+    }
+}
