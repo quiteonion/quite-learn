833e244c0f42fcc7411fd572215db40dfd53ff3b
好喜欢安静啊 <3345973813@qq.com>
2024-03-15 19:47:39 +0800

    循环计算1到100

---
+++ src/A20240315/FOR5050.java
@@ -0,0 +1,11 @@
+package A20240315;
+
+public class FOR5050 {
+    public static void main(String[] args) {
+        int number = 0;
+        for (int i = 1; i <= 100; i++) {
+            number +=i;
+        }
+        System.out.println(number);
+    }
+}
