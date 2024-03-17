8b0292a4797d2b11f9378d0d1ca48b4f4da8f40a
好喜欢安静啊 <3345973813@qq.com>
2024-03-17 23:18:46 +0800

    补充

---
+++ src/A20240317/Card1Test.java
@@ -0,0 +1,18 @@
+package A20240317;
+
+public class Card1Test {
+    public static void main(String[] args) {
+        /**
+         * 一个扑克牌
+         */
+        /*Card c = new Card();
+        System.out.println(c.getCard());*/
+        /**
+         * 多个扑克牌
+         */
+        Card1 c = new Card1();
+        for (int i = 0; i < c.getCard().length; i++) {
+            System.out.println(c.getCard()[i]);
+        }
+    }
+}
