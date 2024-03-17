a3bdf6c640bf5592011721dc09c0420f967bf21e
好喜欢安静啊 <3345973813@qq.com>
2024-03-17 16:42:56 +0800

    OOP扑克牌勒

--- src/A20240317/Card1.java
+++ src/A20240317/Card1.java
@@ -5,13 +5,13 @@ import java.util.Random;
 public class Card1 {
 
 
-        // 一张扑克牌有两个属性： 数字  和  花色
+    // 一张扑克牌有两个属性： 数字  和  花色
 
-        Random r = new Random();
-        /**
-         * 要让数字随机生成 ， 并让他匹配上对应的花色
-         * 随机扑克牌
-         */
+    Random r = new Random();
+    /**
+     * 要让数字随机生成 ， 并让他匹配上对应的花色
+     * 随机扑克牌
+     */
    /* int n = r.nextInt(13) + 1;
     int p = r.nextInt(4);
     private static final String[] arr = {"♥", "♣", "♠", "♦"};*/
@@ -21,9 +21,9 @@ public class Card1 {
         String m = arr[p]+" "+ n;
         return m;
     }*/
-        /**
-         * 扑克牌共有52张数字 2张大小王
-         */
+    /**
+     * 扑克牌共有52张数字 2张大小王
+     */
 
 
 
@@ -39,17 +39,17 @@ public class Card1 {
  * 同一数字花色不能有重复
  * @return
  *//*
-         */
-        /*
+     */
+    /*
 
 
-         *//*
-         */
-        /**
-         * 方法一：创建扑克牌后再打乱花色
-         *
-         * @return
-         *//*
+     *//*
+     */
+    /**
+     * 方法一：创建扑克牌后再打乱花色
+     *
+     * @return
+     *//*
 
     private String[] setCard() {
 
@@ -75,42 +75,55 @@ public class Card1 {
 
 */
 
-        private static final String[] arrCard;
-        private static final String[] arrDecors = {"♥", "♣", "♠", "♦"};
-        /**
-         * 同一数字花色不能有重复
-         * @return
-         */
+    private static final String[] arrCard;
+    private static final String[] arrDecors = {"♥", "♣", "♠", "♦"};
+    /**
+     * 同一数字花色不能有重复
+     * @return
+     */
 
 
-        /**
-         * 方法一：创建扑克牌后再打乱花色
-         *
-         * @return
-         */
-        static {
-            arrCard = new String[52];
-            int k = 0;
+    /**
+     * 方法一：创建扑克牌后再打乱花色
+     *
+     * @return
+     */
+    static {
+        arrCard = new String[52];
+        int k = 0;
 
-            for (int i = 1; i <= 13; i++) {
-                for (int j = 0; j < 4; j++) {
-                    arrCard[k] = arrDecors[j] + " " + i;
-                    k++;
+        for (int i = 1; i <= 13; i++) {
+            for (int j = 0; j < 4; j++) {
+                arrCard[k] = arrDecors[j] + " " + i;
+                k++;
 
-                }
             }
-
-        }
-
-        /**
-         * 改变
-         * 1 、 11 、12、13
-         */
-
-        public String[] getCard() {
-            return arrCard;
         }
 
     }
+
+    public String[] shuffle() {
+        Random r = new Random();
+
+        for (int i = 0; i < arrCard.length; i++) {
+            int number = r.nextInt(arrCard.length);
+            String mun;
+            mun = arrCard[i];
+            arrCard[i] = arrCard[number];
+            arrCard[number] = mun;
+        }
+        return arrCard;
+    }
+
+    /**
+     * 改变
+     * 1 、 11 、12、13
+     */
+
+    public String[] getCard() {
+        return shuffle();
+    }
+
+}
 
 
