da66983b2488848d702bbe58a949ed5b83c4a319
好喜欢安静啊 <3345973813@qq.com>
2024-03-17 16:15:16 +0800

    OOP扑克牌勒

---
+++ src/A20240317/Teacher/DisplayDeck.java
@@ -0,0 +1,13 @@
+package A20240317.Teacher;
+
+public class DisplayDeck {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        for (int suit = Card.DIAMONDS; suit <= Card.SPADES; suit++) {
+            for (int rank = Card.ACE; rank <= Card.KING; rank++) {
+                Card card = deck.getCard(suit, rank);
+                System.out.println(Card.suitToString(card.getSuit()) + " - " + Card.rankToString(card.getRank()));
+            }
+        }
+    }
+}
