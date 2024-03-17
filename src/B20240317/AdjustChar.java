21dba8ac55c9bb873655cbf830876bf50649e945
好喜欢安静啊 <3345973813@qq.com>
2024-03-17 23:43:52 +0800

    堆爆满

---
+++ src/B20240317/AdjustChar.java
@@ -0,0 +1,34 @@
+package B20240317;
+
+import java.util.StringJoiner;
+
+public class AdjustChar {
+    public static void main(String[] args) {
+        StringBuilder SB = new StringBuilder();
+
+        String A = "abcde";
+        String B = "cdeab";
+        /**
+         * 如果两者长度已经不相同就不需要再进行下面判断了
+         */
+        if (A.length() != B.length()){
+            System.out.println("false");
+            System.exit(-1);
+        }
+
+        /**
+         * 如果两者直接相同也无需判断
+         */
+
+        for (int i = 1; i < A.length(); i++) {
+
+            if (A.equals(B)){
+                System.out.println("true");
+                System.exit(-1);
+            }
+            String sp = SB.append(A.substring(1)).append(A.charAt(0)).toString();
+            A = sp;
+        }
+
+    }
+}
