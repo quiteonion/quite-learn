5fc561cd7d26e8a2e421eb2d4b8bc9d2ce73cd57
好喜欢安静啊 <3345973813@qq.com>
2024-03-19 16:39:51 +0800

    生成验证码-3
    随机数组内的元素，并放入StringBuilder的容器内输出

--- src/B20240319/Captcha.java
+++ src/B20240319/Captcha.java
@@ -1,8 +1,9 @@
 package B20240319;
 
+import java.util.Random;
+
 public class Captcha {
     public static void main(String[] args) {
-        StringBuilder SB = new StringBuilder();
         char[] arrChar = new char[52];
         //创建一个字符数组
         /**
@@ -17,11 +18,38 @@ public class Captcha {
             arrChar[i] = (char) ('A'+i-26);
         }
 
-        for (int i = 0; i < arrChar.length; i++) {
+       /* for (int i = 0; i < arrChar.length; i++) {
             System.out.print(arrChar[i]+" ");
-        }
+        }*/
         int[] arrNumber = {1,2,3,4,5,6,7,8,9,0};
 
         //至此，创建数组完毕
+
+        Random r = new Random();
+        StringBuilder SB = new StringBuilder();
+        Boolean flag = true;
+        //由于是4个字母 ， 字母就只使用4次循环
+        //某次循环中会自动加上数字
+        for (int i = 0; i < 4; i++) {
+            int rd = r.nextInt(arrChar.length);
+            int mun = r.nextInt(arrNumber.length);
+            int nnn = r.nextInt(2);
+
+            //随机放入数字
+            //与随机索引进行交换
+            //或者在这里放入随机字符就能够进行添加
+            SB.append(arrChar[rd]);
+            if (nnn == 1){
+                if (flag){
+                    SB.append(arrNumber[mun]);
+                    flag = false;
+                }
+            }
+
+        }
+
+        System.out.println(SB);
+
+
     }
 }
