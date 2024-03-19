f9f8462c56853bfd1ea6fdc4d4bd5ee47ca05551
好喜欢安静啊 <3345973813@qq.com>
2024-03-19 16:19:16 +0800

    生成验证码-1
    如何创建一个字母的字符数组

--- src/B20240319/Captcha.java
+++ src/B20240319/Captcha.java
@@ -5,5 +5,11 @@ public class Captcha {
         StringBuilder SB = new StringBuilder();
         char[] arr = new char[52];
         //创建一个字符数组
+        /**
+         * 错误的创建
+         * arr = {'a','b','c','d'.............,'Z'};
+         * 笨而且低效
+         */
+
     }
 }
