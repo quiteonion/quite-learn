b9f4d2c0e18f728a3efff1b5d26fc07a69b09435
好喜欢安静啊 <3345973813@qq.com>
2024-03-19 16:13:27 +0800

    生成验证码-0

---
+++ src/B20240319/Captcha.java
@@ -0,0 +1,9 @@
+package B20240319;
+
+public class Captcha {
+    public static void main(String[] args) {
+        StringBuilder SB = new StringBuilder();
+        char[] arr = new char[52];
+        //创建一个字符数组
+    }
+}
