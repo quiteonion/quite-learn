ca33385b55dd0ba7fd882d4e6e882f738e8f0d56
好喜欢安静啊 <3345973813@qq.com>
2024-03-20 20:39:32 +0800

    银行模型 - 6
    书写菜单方法

--- src/B20240320/Bank.java
+++ src/B20240320/Bank.java
@@ -8,7 +8,7 @@ public class Bank {
     private int age;
     private int id;
     private int password;
-    private  double money;
+    private double money;
     private static final int pPassword = 1234567;
 
 
@@ -26,8 +26,8 @@ public class Bank {
 
     //用户姓名输入
     //用户姓名的获取
-    public String getName(int pPassword) {
-        if (pPassword != this.password){
+    private String getName(int pPassword) {
+        if (pPassword != this.password) {
             System.out.println("您的管理员密码错误！");
 
         }
@@ -37,7 +37,7 @@ public class Bank {
 
     private void setName() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("请输入您的姓名:"+"  ");
+        System.out.print("请输入您的姓名:" + "  ");
         String name = sc.next();
         this.name = name;
     }
@@ -47,7 +47,7 @@ public class Bank {
      *
      * @return age
      */
-    public int getAge() {
+    private int getAge() {
         return age;
     }
 
@@ -57,7 +57,7 @@ public class Bank {
      * @param age
      */
     private void setAge(int age) {
-        if (age<0||age>200){
+        if (age < 0 || age > 200) {
             System.out.println("您输入的年龄有问题，请重新输入：");
             soutAge();
         }
@@ -73,24 +73,8 @@ public class Bank {
      *
      * @return id
      */
-    public int getId() {
+    private int getId() {
         return id;
-    }
-
-
-    private void setId() {
-        Random r = new Random();
-        StringBuilder SB = new StringBuilder();
-        for (int i = 0; i < 5; i++) {
-            int id = r.nextInt(10);
-            SB.append(id);
-        }
-        this.id = Integer.parseInt(SB.toString());
-    }
-
-
-    public double getMoney() {
-        return money;
     }
 
 
@@ -98,7 +82,7 @@ public class Bank {
         return "抱歉，您没有查看密码的权限";
     }
 
-    public void setPassword(int password) {
+    private void setPassword(int password) {
         this.password = password;
     }
 
@@ -108,7 +92,7 @@ public class Bank {
     }
 
 
-    public int getPassword(int number) {
+    private int getPassword(int number) {
         if (this.password == number) {
             return this.password;
         }
@@ -117,7 +101,50 @@ public class Bank {
     }
 
 
-    public void fetchMoney() {
+
+    public void menu() {
+        System.out.println("1、创建账户");
+        System.out.println("2、存钱");
+        System.out.println("3、取钱");
+        System.out.println("4、更多");
+    }
+
+    public void synthesis() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("请选择您要办理的业务：");
+            menu();
+
+            int number = sc.nextInt();
+            switch (number) {
+                case 1:
+                    newAccount();
+                    continue;
+                case 2:
+                    setMoney();
+                    continue;
+                case 3:
+                    getMoney();
+                case 4:
+                    more();
+                    continue;
+                default:
+                    System.out.println("您输入有问题，请重新输入");
+
+            }
+
+        }
+    }
+
+    private void more() {
+    }
+
+    private void getMoney() {
+    }
+
+    private void setMoney() {
         if (money < 0) {
             System.out.println("您输入的钱可能有问题,怀疑您在进行非法操作 ， 将会 删除您的所有资金");
             this.money = 0;
@@ -126,10 +153,20 @@ public class Bank {
         this.money = money;
     }
 
-    public void menu(){
-        System.out.println("请选择您要办理的业务：");
+
+
+    private Bank[] newAccount() {
+        setName();
     }
 
-
+    private void setPassword() {
+        Random r = new Random();
+        StringBuilder SB = new StringBuilder();
+        for (int i = 0; i < 5; i++) {
+            int id = r.nextInt(10);
+            SB.append(id);
+        }
+        this.id = Integer.parseInt(SB.toString());
+    }
 }
 
