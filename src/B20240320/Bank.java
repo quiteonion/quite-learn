e284f55c6b700b3411bade42e4fecd5df3b137f0
好喜欢安静啊 <3345973813@qq.com>
2024-03-20 19:58:06 +0800

    银行模型 - 1
    在对用户的数据进行编写时，我想到，密码如果直接获取是不可以的，因为若是非用户要获取密码就会显得不安全，则我加入管理员，让管理员输入密码来获取用户的密码，加大安全性！其次，用户如果存储金钱时输入的金额为负数，则要返回用户输入错误的提示

--- src/B20240320/Bank.java
+++ src/B20240320/Bank.java
@@ -9,7 +9,7 @@ public class Bank {
     private int id;
     private int password;
     private  double money;
-
+    private static final int pPassword = 1234567;
 
 
     public Bank() {
@@ -28,6 +28,124 @@ public class Bank {
         this.id = id;
         this.password = password;
         this.money = money;
+    }
+
+    /**
+     * 获取
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     *
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     *
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     *
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     *
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     *
+     * @return money
+     */
+    public double getMoney() {
+        return money;
+    }
+
+    /**
+     * 设置
+     *
+     * @param money
+     */
+
+
+    /**
+     * 获取
+     * @return password
+     */
+    public String getPassword() {
+        return "抱歉，您没有查看密码的权限";
+    }
+
+    /**
+     * 设置
+     * @param password
+     */
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    /**
+     * 设置
+     * @param money
+     */
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+
+    public int getPassword(int number) {
+        if (this.password == number) {
+            return this.password;
+        }
+        System.out.println("您输入的管理员密码错误！");
+        return 0;
+    }
+
+    public void fetchMoney() {
+        if (money < 0) {
+            System.out.println("您输入的钱可能有问题,怀疑您在进行非法操作 ， 将会 删除您的所有资金");
+            this.money = 0;
+            return;
+        }
+        this.money = money;
+    }
+
+
+    public void load(){
+        Scanner sc = new Scanner(System.in);
+
     }
 
 }
