263ae8e6624c96980b6cfead1864a06170d1e159
好喜欢安静啊 <3345973813@qq.com>
2024-03-22 16:46:44 +0800

    集合练习二 -- 基本数据类型对应的包装类

---
+++ src/A20240322/Test2.java
@@ -0,0 +1,15 @@
+package A20240322;
+
+import java.util.ArrayList;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ArrayList<Elfs> AL = new ArrayList<>();
+        Elfs e = new Elfs("a",100,'b'); Elfs e1 = new Elfs();
+//        e.setAbility('w');
+        AL.add(e);AL.add(e1);
+        System.out.println(AL);
+        System.out.println(AL.get(0));
+        System.out.println(AL.get(0).getAbility());
+    }
+}
