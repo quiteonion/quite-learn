8cf7370baacde0d1cfae684783e05d295c077bc5
好喜欢安静啊 <3345973813@qq.com>
2024-03-23 18:56:11 +0800

    关于重写 -- 返回值 为引用数据类型且不同时

--- src/B20240323/Test3.java
+++ src/B20240323/Test3.java
@@ -3,28 +3,30 @@ package B20240323;
 public class Test3 {
     public static void main(String[] args) {
         a aa = new a();
-
+        aa.fly();
     }
 }
 
-class A{
-    public void eat(){
+class A {
+    public void eat() {
         System.out.println("eat rice");
     }
 
-    public void play(){
+    public void play() {
         System.out.println("playing");
     }
-    public String fly(){
+
+    public Object fly() {
         return "flying";
     }
-    public double math(){
-        return 1+1;
+
+    public double math() {
+        return 1 + 1;
     }
 }
 
-class a extends A{
-    public void eat(){
+class a extends A {
+    public void eat() {
         System.out.println("eat noodles");
     }
     //public String play(){
@@ -32,8 +34,11 @@ class a extends A{
     //}
     //public void fly(){1
     //}
-    public byte math(){
-        return 1+1;
+    //public byte math(){
+    //    return 1+1;
+    //}
+    public String fly() {
+        return "flying";
     }
 
 }
