2653ed4142a0bb97b008b9ff6ba975529f39f616
好喜欢安静啊 <3345973813@qq.com>
2024-03-23 18:53:18 +0800

    关于重写 -- 返回值 为基本数据类型且不同时

--- src/B20240323/Test3.java
+++ src/B20240323/Test3.java
@@ -18,17 +18,24 @@ class A{
     public String fly(){
         return "flying";
     }
+    public double math(){
+        return 1+1;
+    }
 }
 
 class a extends A{
     public void eat(){
         System.out.println("eat noodles");
     }
-    public String play(){
-        return "playing game";
+    //public String play(){
+    //    return "playing game";
+    //}
+    //public void fly(){1
+    //}
+    public byte math(){
+        return 1+1;
     }
-    public void fly(){1
-    }
+
 }
 
 
