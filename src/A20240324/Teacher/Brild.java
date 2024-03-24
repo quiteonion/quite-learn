85fb20c37e9d6e1f5436994ed00d0afe12f34d96
好喜欢安静啊 <3345973813@qq.com>
2024-03-24 10:25:06 +0800

    小猫 - 使用继承与隐式转换
    改变了代码，但是不改变person
    让一个总类可以调用他子类的名字

---
+++ src/A20240324/Teacher/Brild.java
@@ -0,0 +1,10 @@
+package A20240324.Teacher;
+
+public class Brild extends Animal {
+    public Brild(String name ){
+        super(name);
+    }
+    public void set(String name){
+        super.name = name;
+    }
+}
