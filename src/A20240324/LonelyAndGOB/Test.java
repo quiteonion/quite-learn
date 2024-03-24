2e406fff40e57061032a8d505f717f6d031873d3
好喜欢安静啊 <3345973813@qq.com>
2024-03-24 16:10:18 +0800

    孤独

--- src/A20240324/LonelyAndGOB/Test.java
+++ src/A20240324/LonelyAndGOB/Test.java
@@ -1,4 +1,9 @@
 package A20240324.LonelyAndGOB;
 
 public class Test {
+    public static void main(String[] args) {
+        Student s = new Student("小明");
+        s.lonely("苦酒折柳今相离，无风无月也无你");
+
+    }
 }
