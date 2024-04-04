39314fd7c6b47b24f8bdacb1eb90ba5a8c92ec85
好喜欢安静啊 <3345973813@qq.com>
2024-04-04 13:21:45 +0800

    Flowers are born from buds
    花由芽起

---
+++ src/B20240404/MainClass.java
@@ -0,0 +1,66 @@
+package B20240404;
+
+public class MainClass {
+    private String name;
+    private int id;
+    private int age;
+
+    public MainClass() {
+    }
+
+    public MainClass(String name, int id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
---
+++ src/B20240404/MainSubClass.java
@@ -0,0 +1,5 @@
+package B20240404;
+
+public class MainSubClass extends MainClass {
+
+}
