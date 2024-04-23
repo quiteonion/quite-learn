1e52858d077555b9c7519149246080638d242c81
好喜欢安静啊 <3345973813@qq.com>
2024-04-23 22:07:54 +0800

    关于·JS

--- src/A20240417/AboutJS.java
+++ src/A20240417/AboutJS.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 public class AboutJS {
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://so.gushiwen.org/gushi/shijing.aspx").get();
-        System.out.println(doc.getElementsByTag("spen").text());
+        System.out.println(doc.getElementsByClass("typecont").select("span").size());
+        System.out.println(doc.getElementsByClass("typecont").select("span").text());
+
     }
 }
