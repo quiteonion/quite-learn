b071471e36aeee56e0af4e04db677df1ca5d1a4e
好喜欢安静啊 <3345973813@qq.com>
2024-04-24 20:11:43 +0800

    怎么在网站里查找自己想要的东西

---
+++ src/A20240424/JsoupDemo1.java
@@ -0,0 +1,30 @@
+package A20240424;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class JsoupDemo1 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        Document doc = org.jsoup.Jsoup.connect("http://bbs.xmfish.com/thread-htm-fid-55.html").get();
+        Elements sub = doc.getElementsByClass("subject_t");
+        System.out.println("你要查找的商品是？");
+        String name = sc.nextLine();
+        boolean flag = true;
+        for (int i = 0; i < sub.size(); i++) {
+            String num = sub.get(i).text();
+            if (num.contains(name)){
+                System.out.println(num);
+                System.out.println(sub.get(i));
+                flag = false;
+                break;
+            }
+        }
+        if (flag){
+            System.out.println("抱歉，在第一页没有找到您想要的商品");
+        }
+    }
+}
