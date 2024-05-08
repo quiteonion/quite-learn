828c4b234426f823c2e66f98442d2019ed751cc6
好喜欢安静啊 <3345973813@qq.com>
2024-05-08 20:48:17 +0800

    Test - sum100

---
+++ src/A20240508/TestPack/sum100.java
@@ -0,0 +1,11 @@
+package A20240508.TestPack;
+
+public class sum100 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum +=i;
+        }
+        System.out.println(sum);
+    }
+}
