a48c1f0686c121f6e0bed59605ca365ed735cd7d
好喜欢安静啊 <3345973813@qq.com>
2024-05-12 16:17:31 +0800

    排列引用数据类型 - 使用 behavior 进行书写 -- 发现报错并修改

--- src/A20240512/MakeNewBehavior/MysortUtilState/TestUtil.java
+++ src/A20240512/MakeNewBehavior/MysortUtilState/TestUtil.java
@@ -28,7 +28,7 @@ public class TestUtil {
         MyComparable p;
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j].isLarger(arr[j+1])) {
+                if (arr[j].isLarger(arr[j+1])>0) {
                     p = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = p;
