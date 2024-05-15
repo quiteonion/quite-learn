3922a0a0078c7ada9056eb15d55829ffaf92ad08
好喜欢安静啊 <3345973813@qq.com>
2024-05-15 20:16:34 +0800

    使用了较为笨拙切不好修改的代码

--- src/A20240515/note/MysortUtilState/Test.java
+++ src/A20240515/note/MysortUtilState/Test.java
@@ -3,22 +3,18 @@ package A20240515.note.MysortUtilState;
 
 public class Test {
     public static void main(String[] args) {
-        Student[] studentArr = {new Student("Q", 23), new Student("W", 34), new Student("E", 21), new Student("R", 16), new Student("T", 3)};
-        TV[] TVArr = {new TV("1", 2333), new TV("2", 45245), new TV("3", 57437), new TV("4", 333), new TV("5", 1232),};
-        Room[] roomArr = {new Room("1", 2315513), new Room("2", 143646), new Room("3", 8215513), new Room("4", 657513), new Room("5", 22363)};
-
-//        TestUtil.studentPrint(studentArr);
-//        System.out.println();
-//        TestUtil.studentSort(studentArr);
-//        System.out.println();
-//        TestUtil.studentPrint(studentArr);
-        TestUtil.sort(roomArr);
-        TestUtil.print(roomArr);
+        Student[] studentArr = {new Student("林檎", 23), new Student("齐夏", 34), new Student("楚天秋", 21), new Student("孟州", 16), new Student("钱多多", 3)};
+//        TV[] TVArr = {new TV("1", 2333), new TV("2", 45245), new TV("3", 57437), new TV("4", 333), new TV("5", 1232),};
+//        Room[] roomArr = {new Room("1", 2315513), new Room("2", 143646), new Room("3", 8215513), new Room("4", 657513), new Room("5", 22363)};
+//
+//        TestUtil.sort(roomArr);
+//        TestUtil.print(roomAr);
     }
 }
 
 abstract class MyComparable {
     public abstract int isLarger(MyComparable myComparable);
+    public abstract int isName(MyComparable myComparable);
 }
 
 class Student extends MyComparable {
@@ -43,54 +39,73 @@ class Student extends MyComparable {
         }
         return 0;
     }
-}
-
-class TV extends MyComparable {
-    String name;
-    int price;
-
-    public TV(String name, int price) {
-        this.name = name;
-        this.price = price;
-    }
 
 
     @Override
-    public String toString() {
-        return name + "  " + price;
-    }
-
-
-    @Override
-    public int isLarger(MyComparable myComparable) {
-        if (myComparable instanceof TV tv) {
-            return (int) (this.price - tv.price);
+    public int isName(MyComparable myComparable) {
+        if (myComparable instanceof Student student) {
+            return (int)this.name.charAt(0) -(int) student.name.charAt(0);
         }
         return 0;
     }
 }
 
-class Room extends MyComparable {
-    String name;
-    int area;
+//class TV extends MyComparable {
+//    String name;
+//    int price;
+//
+//    public TV(String name, int price) {
+//        this.name = name;
+//        this.price = price;
+//    }
+//
+//
+//    @Override
+//    public String toString() {
+//        return name + "  " + price;
+//    }
+//
+//
+//    @Override
+//    public int isLarger(MyComparable myComparable) {
+//        if (myComparable instanceof TV tv) {
+//            return (int) (this.price - tv.price);
+//        }
+//        return 0;
+//    }
+//
+//    @Override
+//    public String isName() {
+//        return null;
+//    }
+//}
 
-    public Room(String name, int area) {
-        this.name = name;
-        this.area = area;
-    }
-
-
-    @Override
-    public String toString() {
-        return name + "  " + area;
-    }
-
-
-    @Override
-    public int isLarger(MyComparable myComparable) {
-        if (myComparable instanceof Room room) {
-            return (int) (this.area - room.area);
-        }
-        return 0;
-    }
-}
+//class Room extends MyComparable {
+//    String name;
+//    int area;
+//
+//    public Room(String name, int area) {
+//        this.name = name;
+//        this.area = area;
+//    }
+//
+//
+//    @Override
+//    public String toString() {
+//        return name + "  " + area;
+//    }
+//
+//
+//    @Override
+//    public int isLarger(MyComparable myComparable) {
+//        if (myComparable instanceof Room room) {
+//            return (int) (this.area - room.area);
+//        }
+//        return 0;
+//    }
+//
+//    @Override
+//    public String isName() {
+//        return null;
+//    }
+//}
--- src/A20240515/note/MysortUtilState/TestUtil.java
+++ src/A20240515/note/MysortUtilState/TestUtil.java
@@ -35,31 +35,24 @@ public class TestUtil {
                 }
             }
         }
+    }
 
-//    }
-//    public static void studentSort(ALL[] arr){
-//        ALL p;
-//        for (int i = 0; i < arr.length - 1; i++) {
-//            for (int j = 0; j < arr.length - i - 1; j++) {
-//                if (arr[j].returnNumber() > arr[j + 1].returnNumber()) {
-//                    p = arr[j];
-//                    arr[j] = arr[j + 1];
-//                    arr[j + 1] = p;
-//                }
-//            }
-//        }
-//    }
-//        public static void studentPrint (ALL[]arr){
-//            for (int i = 0; i < arr.length; i++) {
-//                System.out.print(arr[i] + "   ");
-//            }
-//        }
-//        public static void allSort () {
-//
-//        }
-//        public static void allPrint () {
-//
-//        }
-
+    public static void surSort(MyComparable[] arr){
+        //获取姓名首字母的
+        MyComparable p;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j].isName(arr[j+1])>0) {
+                    p = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = p;
+                }
+            }
+        }
+    }
+    public static void surPrint(MyComparable[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "   ");
+        }
     }
 }
