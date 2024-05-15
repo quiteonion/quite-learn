21238be16e135543740338905bdaedad450b85ca
好喜欢安静啊 <3345973813@qq.com>
2024-05-15 20:24:38 +0800

    更改姓名，使得更好的比较

--- src/A20240515/note/MysortUtilState/Test.java
+++ src/A20240515/note/MysortUtilState/Test.java
@@ -3,7 +3,7 @@ package A20240515.note.MysortUtilState;
 
 public class Test {
     public static void main(String[] args) {
-        Student[] studentArr = {new Student("林檎", 23), new Student("齐夏", 34), new Student("楚天秋", 21), new Student("孟州", 16), new Student("钱多多", 3)};
+        Student[] studentArr = {new Student("l林檎", 23), new Student("q齐夏", 34), new Student("q楚天秋", 21), new Student("m孟州", 16), new Student("y余念安", 3)};
         TestUtil.sort(studentArr);
         TestUtil.print(studentArr);
         System.out.println("=========");
