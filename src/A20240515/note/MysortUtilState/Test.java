976d04f7ca01dc7b063c8ab239dc977a7bbfbd1e
好喜欢安静啊 <3345973813@qq.com>
2024-05-15 19:15:23 +0800

    From A20240512 copy

---
+++ src/A20240515/note/MysortUtilState/Test.java
@@ -0,0 +1,96 @@
+package A20240515.note.MysortUtilState;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Student[] studentArr = {new Student("Q", 23), new Student("W", 34), new Student("E", 21), new Student("R", 16), new Student("T", 3)};
+        TV[] TVArr = {new TV("1", 2333), new TV("2", 45245), new TV("3", 57437), new TV("4", 333), new TV("5", 1232),};
+        Room[] roomArr = {new Room("1", 2315513), new Room("2", 143646), new Room("3", 8215513), new Room("4", 657513), new Room("5", 22363)};
+
+//        TestUtil.studentPrint(studentArr);
+//        System.out.println();
+//        TestUtil.studentSort(studentArr);
+//        System.out.println();
+//        TestUtil.studentPrint(studentArr);
+        TestUtil.sort(roomArr);
+        TestUtil.print(roomArr);
+    }
+}
+
+abstract class MyComparable {
+    public abstract int isLarger(MyComparable myComparable);
+}
+
+class Student extends MyComparable {
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + "  " + age;
+    }
+
+
+    @Override
+    public int isLarger(MyComparable myComparable) {
+        if (myComparable instanceof Student student) {
+            return (int) (this.age - student.age);
+        }
+        return 0;
+    }
+}
+
+class TV extends MyComparable {
+    String name;
+    int price;
+
+    public TV(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+    @Override
+    public String toString() {
+        return name + "  " + price;
+    }
+
+
+    @Override
+    public int isLarger(MyComparable myComparable) {
+        if (myComparable instanceof TV tv) {
+            return (int) (this.price - tv.price);
+        }
+        return 0;
+    }
+}
+
+class Room extends MyComparable {
+    String name;
+    int area;
+
+    public Room(String name, int area) {
+        this.name = name;
+        this.area = area;
+    }
+
+
+    @Override
+    public String toString() {
+        return name + "  " + area;
+    }
+
+
+    @Override
+    public int isLarger(MyComparable myComparable) {
+        if (myComparable instanceof Room room) {
+            return (int) (this.area - room.area);
+        }
+        return 0;
+    }
+}
---
+++ src/A20240515/note/MysortUtilState/TestUtil.java
@@ -0,0 +1,65 @@
+package A20240515.note.MysortUtilState;
+
+public class TestUtil {
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "   ");
+        }
+    }
+    public static void sort(int[] arr) {
+        int p;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    p = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = p;
+                }
+            }
+        }
+
+    }
+    public static void print(MyComparable[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "   ");
+        }
+    }
+    public static void sort(MyComparable[] arr) {
+        MyComparable p;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j].isLarger(arr[j+1])>0) {
+                    p = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = p;
+                }
+            }
+        }
+
+//    }
+//    public static void studentSort(ALL[] arr){
+//        ALL p;
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            for (int j = 0; j < arr.length - i - 1; j++) {
+//                if (arr[j].returnNumber() > arr[j + 1].returnNumber()) {
+//                    p = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = p;
+//                }
+//            }
+//        }
+//    }
+//        public static void studentPrint (ALL[]arr){
+//            for (int i = 0; i < arr.length; i++) {
+//                System.out.print(arr[i] + "   ");
+//            }
+//        }
+//        public static void allSort () {
+//
+//        }
+//        public static void allPrint () {
+//
+//        }
+
+    }
+}
