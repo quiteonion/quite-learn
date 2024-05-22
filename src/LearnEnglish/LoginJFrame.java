840eb391cc62dead741b7d84d20e6beca51eeefc
好喜欢安静啊 <3345973813@qq.com>
2024-05-22 13:19:29 +0800

    英语学习 -- 登录界面 -- 设置关闭模式...
    默认值为1
    如果是 0 ： 不做任何事情，即使点了关闭也不会关闭
    如果是 1 ： 关闭界面并不会关闭虚拟机
    如果是 2 ： 关闭所有界面才会关闭虚拟机
    如果是 3 ： 关闭任意一个界面都可以关闭虚拟机

--- src/LearnEnglish/LoginJFrame.java
+++ src/LearnEnglish/LoginJFrame.java
@@ -7,11 +7,13 @@ public class LoginJFrame extends JFrame {
         //设置宽高
         this.setSize(400 , 400);
         //设置标题
-        this.setTitle("英语救星 v1.0");
+        this.setTitle("英语救星 登录 v1.0");
         //设置界面置顶
         this.setAlwaysOnTop(true);
         //设置界面在出现时居中
         this.setLocationRelativeTo(null);
+        //设置关闭模式
+        this.setDefaultCloseOperation(3);
 
         //设置让界面显示出来 ， 最好放在最后
         this.setVisible(true);
