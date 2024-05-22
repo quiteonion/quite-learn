f5a8e240da9d962156a3c317d401a30a2222da26
好喜欢安静啊 <3345973813@qq.com>
2024-05-22 20:42:13 +0800

    自定义一个容器类 Xxx -- 满足可以使用 Xxx a = new Xxx("实训室8" , "4台风扇");

---
+++ src/A20240522/Test.java
@@ -0,0 +1,8 @@
+package A20240522;
+
+public class Test {
+    public static void main(String[] args) {
+        Xxx a = new Xxx("实训室8" , "4台风扇");
+
+    }
+}
---
+++ src/A20240522/Xxx.java
@@ -0,0 +1,10 @@
+package A20240522;
+
+public class Xxx {
+    private String name;
+    private String number;
+    public Xxx(String name , String number){
+        this.name = name;
+        this.number = number;
+    }
+}
