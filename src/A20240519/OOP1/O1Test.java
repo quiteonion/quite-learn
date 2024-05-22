a2958c247891bf123fc07dbe42fd0342bf2debf2
好喜欢安静啊 <3345973813@qq.com>
2024-05-22 13:02:31 +0800

    英语学习 -- 登录界面 -- 设置宽高，并且让他显示出来

--- src/A20240519/OOP1/O1Test.java
+++ src/A20240519/OOP1/O1Test.java
@@ -3,6 +3,6 @@ package A20240519.OOP1;
 public class O1Test {
     public static void main(String[] args) {
         O1 o = new O1();
-        o.name
+
     }
 }
--- src/LearnEnglish/LoginJFrame.java
+++ src/LearnEnglish/LoginJFrame.java
@@ -1,4 +1,10 @@
 package LearnEnglish;
 
-public class LoginJFrame {
+import javax.swing.*;
+
+public class LoginJFrame extends JFrame {
+    public LoginJFrame(){
+        this.setSize(400 , 400);
+        this.setVisible(true);
+    }
 }
