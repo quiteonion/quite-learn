72fa1332c48a5a9b8d2ce482a1f04ab433935a2a
好喜欢安静啊 <3345973813@qq.com>
2024-05-22 20:56:29 +0800

    自定义一个容器类 Xxx -- 满足可以使用 Xxx b = new Xxx(); b.set & b.get 方法

--- src/A20240522/Xxx.java
+++ src/A20240522/Xxx.java
@@ -3,8 +3,20 @@ package A20240522;
 public class Xxx {
     private String name;
     private String number;
-    public Xxx(String name , String number){
+
+    public Xxx(String name, String number) {
         this.name = name;
         this.number = number;
+    }
+
+    public void set(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public void get(String name) {
+        if (name.equals(this.name)) {
+            System.out.println(this.number);
+        }
     }
 }
