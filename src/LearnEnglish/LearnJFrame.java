f5dd91760808796100a2d02c5d78559a5de0920d
好喜欢安静啊 <3345973813@qq.com>
2024-05-22 13:28:12 +0800

    英语学习 -- 所有界面 -- 设置和登录界面一样的东西

--- src/LearnEnglish/LearnJFrame.java
+++ src/LearnEnglish/LearnJFrame.java
@@ -1,4 +1,21 @@
 package LearnEnglish;
 
-public class LearnJFrame {
+import javax.swing.*;
+
+public class LearnJFrame extends JFrame {
+    public LearnJFrame() {
+        //设置宽高
+        this.setSize(400,400);
+        //设置标题
+        this.setTitle("英语救星 学习 v1.0");
+        //设置居中
+        this.setLocationRelativeTo(null);
+        //设置始终位于最上层
+        this.setAlwaysOnTop(true);
+        //设置关闭模式
+        this.setDefaultCloseOperation(3);
+
+        //设置是否显示
+        this.setVisible(true);
+    }
 }
--- src/LearnEnglish/RegisterJFrame.java
+++ src/LearnEnglish/RegisterJFrame.java
@@ -1,4 +1,21 @@
 package LearnEnglish;
 
-public class RegisterJFrame {
+import javax.swing.*;
+
+public class RegisterJFrame extends JFrame {
+    public RegisterJFrame(){
+        //设置宽高
+        this.setSize(400,400);
+        //设置标题
+        this.setTitle("英语救星 注册 v1.0");
+        //设置居中
+        this.setAlwaysOnTop(true);
+        //设置始终位于最上层
+        this.setLocationRelativeTo(null);
+        //设置关闭模式
+        this.setDefaultCloseOperation(3);
+
+        //设置是否显示
+        this.setVisible(true);
+    }
 }
