cc43942f391bffe39e1cb17addb60172c243ffbe
好喜欢安静啊 <3345973813@qq.com>
2024-05-22 13:06:50 +0800

    英语学习 -- 登录界面 -- 设置界面标题

--- src/LearnEnglish/LoginJFrame.java
+++ src/LearnEnglish/LoginJFrame.java
@@ -4,7 +4,13 @@ import javax.swing.*;
 
 public class LoginJFrame extends JFrame {
     public LoginJFrame(){
+        //设置宽高
         this.setSize(400 , 400);
+        //设置标题
+        this.setTitle("英语救星 v1.0");
+
+
+        //设置让界面显示出来 ， 最好放在最后
         this.setVisible(true);
     }
 }
