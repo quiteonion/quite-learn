7d2a1b85ede029280d2304ef79a21593a2043fc8
好喜欢安静啊 <3345973813@qq.com>
2024-05-24 19:22:54 +0800

    包装类 -- int & Integer..
    在泛型中 ， 传递的值需要为 Object 类 ， 所以如果要使用的是 int 类型 就需要使用 他的包装类 Integer

--- src/A20240524/Test.java
+++ src/A20240524/Test.java
@@ -2,6 +2,7 @@ package A20240524;
 
 public class Test {
     public static void main(String[] args) {
-        MyMap<String , int> m  =new MyMap<>("aaa" , 2);
+//        MyMap<String , int> m  =new MyMap<>("aaa" , 2);
+        MyMap<String , Integer> m  =new MyMap<>("aaa" , 2);
     }
 }
