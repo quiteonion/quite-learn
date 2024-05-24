37e28229be7966d9a02e1b2d10542acdfaf61d4e
好喜欢安静啊 <3345973813@qq.com>
2024-05-24 19:39:40 +0800

    包装类 -- 所有的基础数据类型都有包装类

--- src/A20240524/Test.java
+++ src/A20240524/Test.java
@@ -3,6 +3,6 @@ package A20240524;
 public class Test {
     public static void main(String[] args) {
 //        MyMap<String , int> m  =new MyMap<>("aaa" , 2);
-        MyMap<String , Integer> m  =new MyMap<>("aaa" , 2);
+        MyMap<String , Integer> m1  =new MyMap<>("aaa" , 2);
     }
 }
