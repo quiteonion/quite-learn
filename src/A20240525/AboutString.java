f33d41f64533cd6a80b7889ced5e63fa58052167
好喜欢安静啊 <3345973813@qq.com>
2024-05-25 10:56:27 +0800

    String 面试题 -- 两个 new 出的 String 类型之间比较 -- 输出 false ，因为比较的是地址值

---
+++ src/A20240525/AboutString.java
@@ -0,0 +1,9 @@
+package A20240525;
+
+public class AboutString {
+    public static void main(String[] args) {
+        String s1 = new String("123");
+        String s2 = new String("123");
+        System.out.println(s1 == s2);
+    }
+}
