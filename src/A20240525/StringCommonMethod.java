83d26fd405d1b34565df57fc72e0cdab4b8f9995
好喜欢安静啊 <3345973813@qq.com>
2024-05-25 16:08:12 +0800

    String 的其他方法 -- equals 比较两个字符串是否相同

--- src/A20240525/StringCommonMethod.java
+++ src/A20240525/StringCommonMethod.java
@@ -5,5 +5,8 @@ public class StringCommonMethod {
         String code = "asagaf";
         String codeUpperCase = code.toUpperCase();
         System.out.println(codeUpperCase);
+        System.out.println("=== === ===");
+        System.out.println(code.equals(codeUpperCase));
+        System.out.println(code.equalsIgnoreCase(codeUpperCase));
     }
 }
