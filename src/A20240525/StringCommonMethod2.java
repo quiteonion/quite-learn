901bc53065ba84f1508b3837b68bd5b8b87479ca
好喜欢安静啊 <3345973813@qq.com>
2024-05-25 17:01:21 +0800

    String 的其他方法

--- src/A20240525/StringCommonMethod2.java
+++ src/A20240525/StringCommonMethod2.java
@@ -2,13 +2,22 @@ package A20240525;
 
 public class StringCommonMethod2 {
     public static void main(String[] args) {
+        //把手机号码隐藏起来
         String Phone = "17759661372";
         System.out.println(Phone.replace(Phone.substring(3, 7), "****"));
+
+
         System.out.println("=== === ===");
+
+        //把骂人的话隐藏起来
         String s1 = "sb";
         String s2 = "epsbifsbbsdjdvssbjhbsbsbfjdoissbfhsb";
         System.out.println(s2.replace(s1, "**"));
+
+
         System.out.println("=== === ===");
+
+        //把骂人的话隐藏起来
         String[] words = new String[]{"包子","傻逼","良心"};
         String s3 = "早上买的包子很好吃 ， 但是老板说他是傻逼 ， 但是我还是觉得他很良心";
         for (int i = 0; i < words.length; i++) {
