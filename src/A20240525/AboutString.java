af4c14c57e49b9047c4a2581932fd7d120ab694f
好喜欢安静啊 <3345973813@qq.com>
2024-05-25 12:03:14 +0800

    String 面试题 -- String 类型之间比较 -- 由直接赋值的 String 会被放入常量池...
    如果被使用会在常量池中 ， 而调用的 变量则会 在堆空间中被调用 ， 所以两者之间会产生不同

--- src/A20240525/AboutString.java
+++ src/A20240525/AboutString.java
@@ -14,5 +14,24 @@ public class AboutString {
         System.out.println(s3 == s4);
 
         System.out.println("=== === ===");
+
+        String q1 = "123";
+        String q2 = "456";
+        String q3 = "123456";
+        String q4 = "123" + "456";
+        String q5 = q1 + "456";
+        String q6 = "123" + q2;
+        String q7 = q1 + q2;
+        System.out.println(q3 ==q4);
+        System.out.println(q3 ==q5);
+        System.out.println(q3 ==q6);
+        System.out.println(q3 ==q7);
+        System.out.println(q4 ==q5);
+        System.out.println(q5 ==q6);
+        System.out.println(q6 ==q7);
+
+        System.out.println("=== === ===");
+
+
     }
 }
