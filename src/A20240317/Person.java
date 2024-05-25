e5f01712813bdf8d2648dacee2f5c71186284959
好喜欢安静啊 <3345973813@qq.com>
2024-05-25 13:46:12 +0800

    A20240317 -- 修改与补充

--- src/A20240317/Person.java
+++ src/A20240317/Person.java
@@ -4,7 +4,7 @@ public class Person {
     String name;
     int age;
     public void eat(){
-        System.out.println("吃饭吃饭");
+        System.out.println("吃饭吃饭000");
     }
     public void qq(){
         System.out.println("000000");
