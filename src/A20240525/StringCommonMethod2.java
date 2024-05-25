f1bbedf923b7f448c1ee0b28925bacd6f9d9f488
好喜欢安静啊 <3345973813@qq.com>
2024-05-25 16:34:38 +0800

    String 的其他方法

---
+++ src/A20240525/StringCommonMethod2.java
@@ -0,0 +1,19 @@
+package A20240525;
+
+public class StringCommonMethod2 {
+    public static void main(String[] args) {
+        String Phone = "17759661372";
+        System.out.println(Phone.replace(Phone.substring(3, 7), "****"));
+        System.out.println("=== === ===");
+        String s1 = "sb";
+        String s2 = "epsbifsbbsdjdvssbjhbsbsbfjdoissbfhsb";
+        System.out.println(s2.replace(s1, "**"));
+        System.out.println("=== === ===");
+        String[] words = new String[]{"包子","傻逼","良心"};
+        String s3 = "早上买的包子很好吃 ， 但是老板说他是傻逼 ， 但是我还是觉得他很良心";
+        for (int i = 0; i < words.length; i++) {
+            s3 =  s3.replace(words[i] , "**");
+        }
+        System.out.println(s3);
+    }
+}
