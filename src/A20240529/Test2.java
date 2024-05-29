7b55888f1528d7727c27ec343f78c0d7b33d512f
好喜欢安静啊 <3345973813@qq.com>
2024-05-29 19:40:23 +0800

    File - about File

--- src/A20240529/Test2.java
+++ src/A20240529/Test2.java
@@ -1,7 +1,19 @@
 package A20240529;
 
+import java.io.File;
+
 public class Test2 {
     public static void main(String[] args) {
+        File f1 = new File("src/A20240529");
+        System.out.println( f1.toString() );
+        System.out.println( f1.getName() );
+        System.out.println( f1.isFile() );
+        System.out.println( f1.isDirectory() );
+        String[] list = f1.list();
+        System.out.println(list);
 
+        System.out.println("=== === ===");
+
+        File f2 = new File("src/A20240529/Test.java");
     }
 }
