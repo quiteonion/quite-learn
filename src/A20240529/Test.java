21893b48e8395166679d3ad77c393ce43f4965df
好喜欢安静啊 <3345973813@qq.com>
2024-05-29 19:13:29 +0800

    String - indexOf -- 返回文件后缀名

---
+++ src/A20240529/Test.java
@@ -0,0 +1,12 @@
+package A20240529;
+
+public class Test {
+    public static void main(String[] args) {
+        String fileName1 = "MyStringBuilder.java";
+        String fileName2 = "My.StringBuilder.java";
+        String fileName3 = "abc.txt";
+        String fileName4 = "xyz_.80_i.png";
+
+
+    }
+}
