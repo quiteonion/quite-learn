00f5ea949b803c757165ac381f9a27c5fe2d1368
好喜欢安静啊 <3345973813@qq.com>
2024-05-29 19:29:48 +0800

    String - indexOf -- 返回文件后缀名

--- src/A20240529/Test.java
+++ src/A20240529/Test.java
@@ -7,6 +7,15 @@ public class Test {
         String fileName3 = "abc.txt";
         String fileName4 = "xyz_.80_i.png";
 
+        while (true){
+            int i = fileName4.indexOf(".");
+            if (fileName4.indexOf("." , i+1) != -1){
+                int j = fileName4.indexOf("." , i+1);
+                i = j;
+            }
+            System.out.println(fileName4.substring(i));
+            break;
+        }
 
     }
 }
