40aba8a23f82b8f08b381d1fa9ffb3e7a9b30472
好喜欢安静啊 <3345973813@qq.com>
2024-05-29 19:41:52 +0800

    File - about File

--- src/A20240529/Test2.java
+++ src/A20240529/Test2.java
@@ -1,6 +1,7 @@
 package A20240529;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Test2 {
     public static void main(String[] args) {
@@ -11,9 +12,17 @@ public class Test2 {
         System.out.println( f1.isDirectory() );
         String[] list = f1.list();
         System.out.println(list);
+        System.out.println(Arrays.toString(list));
 
         System.out.println("=== === ===");
 
         File f2 = new File("src/A20240529/Test.java");
+        System.out.println( f2.toString() );
+        System.out.println( f2.getName() );
+        System.out.println( f2.isFile() );
+        System.out.println( f2.isDirectory() );
+        String[] list2 = f2.list();
+        System.out.println(list2);
+        System.out.println(Arrays.toString(list2));
     }
 }
