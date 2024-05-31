f6c8632803beb465bdb3b33bc9130250510c3b95
好喜欢安静啊 <3345973813@qq.com>
2024-05-31 20:17:10 +0800

    ArrayList - ArrayList 的父类 AbstractCollection 重写了 toString 方法

---
+++ src/A20240531/AboutCollection.java
@@ -0,0 +1,15 @@
+package A20240531;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class AboutCollection {
+    public static void main(String[] args) {
+        Collection<String> arr = new ArrayList<>();
+        arr.add("1");
+        arr.add("2");
+        arr.add("3");
+        System.out.println(arr);
+    }
+}
