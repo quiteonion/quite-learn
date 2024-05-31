56829ff07dfe091f97e34f58759014efa0b00ade
好喜欢安静啊 <3345973813@qq.com>
2024-05-31 19:46:15 +0800

    ArrayList - ArrayList 的初始长度为 0 ...
    ArrayList 的初始长度： 0
    StringBuilder 的初始长度： 16

---
+++ src/A20240531/AboutArrayList.java
@@ -0,0 +1,10 @@
+package A20240531;
+
+import java.util.ArrayList;
+
+public class AboutArrayList {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<String> arrayList = new ArrayList<>();
+    }
+}
