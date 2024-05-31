19173a4cd5c59640b9cf32ec8b67c271770aff82
好喜欢安静啊 <3345973813@qq.com>
2024-05-31 21:14:21 +0800

    ArrayList - contains -- 查找是否拥有要查找的值

---
+++ src/A20240531/AboutCollection3.java
@@ -0,0 +1,16 @@
+package A20240531;
+
+import java.util.ArrayList;
+
+public class AboutCollection3 {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("1");
+        arr.add("2");
+        arr.add("3");
+        System.out.println(arr.contains("2"));
+        System.out.println("=== === ===");
+
+
+    }
+}
