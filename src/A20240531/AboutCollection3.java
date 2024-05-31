213c3df51a233f91aa2989e0c27597221fff8cec
好喜欢安静啊 <3345973813@qq.com>
2024-05-31 21:15:30 +0800

    ArrayList - contains -- 如果自己书写的类型 就会导致无法判断怎么解决

--- src/A20240531/AboutCollection3.java
+++ src/A20240531/AboutCollection3.java
@@ -11,6 +11,23 @@ public class AboutCollection3 {
         System.out.println(arr.contains("2"));
         System.out.println("=== === ===");
 
+        ArrayList<Student> arr1 = new ArrayList<>();
+        arr1.add(new Student("a" , 18));
+        arr1.add(new Student("b" , 19));
+        arr1.add(new Student("c" , 20));
+        System.out.println(arr1.contains(new Student("b" , 19)));
 
     }
 }
+class Student{
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    }
+
