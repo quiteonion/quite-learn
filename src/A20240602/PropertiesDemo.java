011205ecbf4c06a798e25c7317b27bbad0a03d97
好喜欢安静啊 <3345973813@qq.com>
2024-06-02 16:57:09 +0800

    Map - Hashtable -  properties - properties 是 Hashtable 的一个 子类，基础功能都有

---
+++ src/A20240602/PropertiesDemo.java
@@ -0,0 +1,13 @@
+package A20240602;
+
+import java.util.Properties;
+
+public class PropertiesDemo {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("asd","oiy");
+        properties.put("ags","hdgn");
+        properties.put("fdg","cvnnk");
+        System.out.println(properties);
+    }
+}
