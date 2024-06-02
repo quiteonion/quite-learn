665449bb770264af2d74a41aee2f2f6b708eb519
好喜欢安静啊 <3345973813@qq.com>
2024-06-02 09:53:10 +0800

    String.equalsIgnoreCase - 底层是 使用了 toLowerCase() 和 toUpperCase()

---
+++ src/A20240602/AboutEquals.java
@@ -0,0 +1,12 @@
+package A20240602;
+
+public class AboutEquals {
+    public static void main(String[] args) {
+        String a = "ABC";
+        String b = "abc";
+        a.equalsIgnoreCase(b);
+        System.out.println(a.toLowerCase());
+        a.toUpperCase();
+    }
+
+}
