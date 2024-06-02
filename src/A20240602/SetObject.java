97711453957819007c357af668af8c7d3024ce6b
好喜欢安静啊 <3345973813@qq.com>
2024-06-02 14:17:19 +0800

    Set - set -- equals 重写 - 单单重写一个 equals 还不够，还需要重写 hashCode

---
+++ src/A20240602/SetObject.java
@@ -0,0 +1,49 @@
+package A20240602;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetObject {
+    public static void main(String[] args) {
+        Set<Student> arr = new HashSet<>();
+        arr.add(new Student("1", 1));
+        arr.add(new Student("2", 2));
+        arr.add(new Student("3", 3));
+
+        arr.add(new Student("3", 3));
+        System.out.println(arr.size());
+        System.out.println(arr);
+        System.out.println("=== === === ===");
+        System.out.println(arr.contains(new Student("3", 3)));
+
+    }
+}
+
+class Student  {
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Student student) {
+                return this.name.equals(student.name) && this.age == student.age;
+        }
+        return false;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
