f8cf5130af65eef85afa49f0028147ca9bf76336
好喜欢安静啊 <3345973813@qq.com>
2024-06-03 21:02:33 +0800

    Math - abs -- abs 的能力便是取得绝对值

---
+++ src/B20240603/MathDemo.java
@@ -0,0 +1,11 @@
+package B20240603;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        //众所周知 ： abs 是 Math 类中典型的代表，简单来讲就是取绝对值，但是有没有想过：如果这个数字比 long 类型的最小值还小呢？那该怎么办？
+        System.out.println(Math.abs(-3247061002160697142L));
+    }
+    public static String abs(){
+
+    }
+}
