9767ba21d10e934d8fcb15559e9e76c0bf3bfa18
好喜欢安静啊 <3345973813@qq.com>
2024-06-03 22:00:35 +0800

    Math - max -- 判断最大值

---
+++ src/B20240603/MathMax.java
@@ -0,0 +1,13 @@
+package B20240603;
+
+public class MathMax {
+    public static void main(String[] args) {
+        /***
+         * max
+         * max 判断最大值
+         * 让他看看哪个更大 O.o
+         */
+        System.out.println(Math.max(1, 4));
+        System.out.println(Math.max(7, 2));
+    }
+}
