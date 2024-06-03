643a02b8c6657a0913672fa6311c5a2d1c5bbad5
好喜欢安静啊 <3345973813@qq.com>
2024-06-03 14:44:18 +0800

    properties

--- src/A20240602/PropertiesDemo.java
+++ src/A20240602/PropertiesDemo.java
@@ -1,13 +1,19 @@
 package A20240602;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
-        properties.put("asd","oiy");
-        properties.put("ags","hdgn");
-        properties.put("fdg","cvnnk");
+//        properties.put("asd","oiy");
+//        properties.put("ags","hdgn");
+//        properties.put("fdg","cvnnk");
+        File file = new File("src/A20240602/config.properties");
+        properties.load(new FileInputStream(file));
         System.out.println(properties);
     }
 }
