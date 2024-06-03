c8fe4d14a9f632e6f597b289fd55d2e5999afb2d
好喜欢安静啊 <3345973813@qq.com>
2024-06-03 21:33:06 +0800

    Math - round -- 四舍五入

---
+++ src/B20240603/MathRound.java
@@ -0,0 +1,14 @@
+package B20240603;
+
+public class MathRound {
+    public static void main(String[] args) {
+        /**
+         *round
+         * Math 类 掌管四舍五入的神
+         */
+        for (double i = 0; i < 1; ) {
+            System.out.println("放入" + i + "输出" + Math.round(i));
+            i += 0.1;
+        }
+    }
+}
