6d6e2cd72142a2ede92f61acba88f356d36e3785
好喜欢安静啊 <3345973813@qq.com>
2024-06-04 15:15:46 +0800

    Math - pow -- 获取 前者的后者次幂

---
+++ src/B20240603/MathPow.java
@@ -0,0 +1,13 @@
+package B20240603;
+
+public class MathPow {
+    public static void main(String[] args) {
+        /**
+         * pow
+         * 有几个你，那便是幂
+         */
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.pow(2, 0.3));
+        System.out.println(Math.pow(4,-2));
+    }
+}
