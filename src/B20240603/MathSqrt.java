931cdaf97d3c222d36da1f87d7eb487e68d04aea
好喜欢安静啊 <3345973813@qq.com>
2024-06-04 16:54:34 +0800

    Math - sqrt -- 给一个数字开 根号

---
+++ src/B20240603/MathSqrt.java
@@ -0,0 +1,15 @@
+package B20240603;
+
+public class MathSqrt {
+    public static void main(String[] args) {
+        /**
+         * sqrt
+         * 1 ： 给他开了！！！
+         * sqrt : 遵命！
+         * 4 ： 不要啊！！！
+         * Math.sqrt(4).....
+         * 2 ： 啊？？？？
+         */
+        System.out.println(Math.sqrt(4));
+    }
+}
