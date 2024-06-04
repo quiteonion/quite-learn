581298ac4ea9e52abf0f4f0dca4688ab9ad373dc
好喜欢安静啊 <3345973813@qq.com>
2024-06-04 15:12:39 +0800

    Math - min -- 判断最小值

---
+++ src/B20240603/MathMin.java
@@ -0,0 +1,13 @@
+package B20240603;
+
+public class MathMin {
+    public static void main(String[] args) {
+        /**
+         * min
+         * 两个之间哪一个最小，他一看就知道
+         * 取两数之间的最小值
+         */
+        System.out.println(Math.min(3, 9));
+        System.out.println(Math.min(9752, 31));
+    }
+}
