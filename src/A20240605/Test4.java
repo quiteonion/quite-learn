c9dc5ee8cb13eee07abc2f3f60cd5bc53139cba4
好喜欢安静啊 <3345973813@qq.com>
2024-06-05 20:42:18 +0800

    异常 - java.lang.OutOfMemoryError -- 堆爆满

--- src/A20240605/Test4.java
+++ src/A20240605/Test4.java
@@ -1,4 +1,12 @@
 package A20240605;
 
 public class Test4 {
+    public static void main(String[] args) {
+        /**
+         * Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+         * 例外事件
+         * 堆爆满
+         */
+        String[] arr = new String[(int)(Math.pow(1024,3))];
+    }
 }
