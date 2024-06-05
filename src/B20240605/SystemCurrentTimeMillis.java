b97fb1c24fdef3a234e1b36f787c6fbd5cebd28c
好喜欢安静啊 <3345973813@qq.com>
2024-06-05 13:10:26 +0800

    System - currentTimeMillis -- 计算当前时间与 1970 年 1 月 1 日 0 点 0 分 0 时 0 秒 的时间间隔

---
+++ src/B20240605/SystemCurrentTimeMillis.java
@@ -0,0 +1,11 @@
+package B20240605;
+
+public class SystemCurrentTimeMillis {
+    public static void main(String[] args) {
+        /**
+         * currentTimeMillis
+         * 计算当前时间与 1970 年 1 月 1 日 0 点 0 分 0 时 0 秒 的时间间隔
+         */
+        System.out.println(System.currentTimeMillis());
+    }
+}
