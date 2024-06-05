5c963536d65e7e924c0d701a2ce437cb260a9b96
好喜欢安静啊 <3345973813@qq.com>
2024-06-05 12:53:48 +0800

    System - exit -- 停止虚拟机...
    如果是0 ： 正常停止
    如果不是0 ： 异常停止

---
+++ src/B20240605/SystemExit.java
@@ -0,0 +1,15 @@
+package B20240605;
+
+public class SystemExit {
+    public static void main(String[] args) {
+        /**
+         * exit
+         * 停止虚拟机
+         * 如果是0 ： 正常停止
+         * 如果不是0 ： 异常停止
+         */
+        System.out.println("123456");
+        System.exit(0);
+        System.out.println(678900);
+    }
+}
