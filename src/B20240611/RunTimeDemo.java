ed6cc75eeadd9ad31a4443fe45a92f436d0d761d
好喜欢安静啊 <3345973813@qq.com>
2024-06-11 16:57:41 +0800

    Runtime - availableProcessors -- 查看自己的 CPU 线程数

--- src/B20240611/RunTimeDemo.java
+++ src/B20240611/RunTimeDemo.java
@@ -4,7 +4,7 @@ public class RunTimeDemo {
     public static void main(String[] args) {
 //        Runtime runtime = new Runtime();
         Runtime runtime = Runtime.getRuntime();
-
+        System.out.println(runtime.availableProcessors());
         runtime.exit(0);
     }
 }
