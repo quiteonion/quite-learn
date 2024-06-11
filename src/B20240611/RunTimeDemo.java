5e01baa3ba6155da324bb8c10a00487db6dc2db2
好喜欢安静啊 <3345973813@qq.com>
2024-06-11 17:01:41 +0800

    Runtime - maxMemory -- 虚拟机能从系统获得的总内存的大小

--- src/B20240611/RunTimeDemo.java
+++ src/B20240611/RunTimeDemo.java
@@ -4,7 +4,8 @@ public class RunTimeDemo {
     public static void main(String[] args) {
 //        Runtime runtime = new Runtime();
         Runtime runtime = Runtime.getRuntime();
-        System.out.println(runtime.availableProcessors());
+        System.out.println("CPU 线程数： " + runtime.availableProcessors());
+        System.out.println("JVM 能从系统中获取的总内存的大小： " + runtime.maxMemory() + " size = " + runtime.maxMemory() / 1024 + " KB = " + runtime.maxMemory() / 1024 / 1024 + " MB");
         runtime.exit(0);
     }
 }
