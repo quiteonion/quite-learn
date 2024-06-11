e3b7c6305f75336d89905aae75aeb12c68208f3e
好喜欢安静啊 <3345973813@qq.com>
2024-06-11 16:31:45 +0800

    Runtime - about "new" -- Runtime 内部提供了一个静态方法...
    这个方法直接帮助我们 new 了一个 Runtime 的对象

--- src/B20240611/RunTimeDemo.java
+++ src/B20240611/RunTimeDemo.java
@@ -2,7 +2,8 @@ package B20240611;
 
 public class RunTimeDemo {
     public static void main(String[] args) {
-        Runtime runtime = new Runtime();
+//        Runtime runtime = new Runtime();
+        Runtime runtime = Runtime.getRuntime();
 
     }
 }
