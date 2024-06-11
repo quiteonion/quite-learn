352a04060799d20daee26a122aa5edd13577d569
好喜欢安静啊 <3345973813@qq.com>
2024-06-11 16:18:59 +0800

    System - arraycopy -- 数组的扩展...
    System.arraycopy(老数组， 从哪里开始复制， 新数组， 从哪里开始粘贴， 复制个数)

--- src/B20240611/SystemArrayCopy.java
+++ src/B20240611/SystemArrayCopy.java
@@ -1,7 +1,12 @@
 package B20240611;
 
+import java.util.Arrays;
+
 public class SystemArrayCopy {
     public static void main(String[] args) {
-
+        int[] arr1 = {1,2,3,4,5,6,7,8,9};
+        int[] arr2 = new int[10];
+        System.arraycopy(arr1 , 0 , arr2 ,0 , 9 );
+        System.out.println(Arrays.toString(arr2));
     }
 }
