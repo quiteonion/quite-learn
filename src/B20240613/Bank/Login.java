6637e0bbf6d16d817bd7cbf994cd589d367af4ef
好喜欢安静啊 <3345973813@qq.com>
2024-06-14 15:40:05 +0800

    银行 - 登录 -- 设置登录界面的标题

--- src/B20240613/Bank/Login.java
+++ src/B20240613/Bank/Login.java
@@ -6,6 +6,7 @@ public class Login {
     public Login(){
         JFrame loginjFrame = new JFrame();
         loginjFrame.setSize(1050,800);
+        loginjFrame.setTitle("银行 - 登录界面");
 
         loginjFrame.setVisible(true);
     }
