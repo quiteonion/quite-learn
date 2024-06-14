1813a6479fd544c430e3ce02442c3ad0dc1b6f29
好喜欢安静啊 <3345973813@qq.com>
2024-06-14 16:09:55 +0800

    银行 - 登录 -- 菜单的构建

--- src/B20240613/Bank/Login.java
+++ src/B20240613/Bank/Login.java
@@ -2,24 +2,48 @@ package B20240613.Bank;
 
 import javax.swing.*;
 
-public class Login {
+public class Login extends JFrame{
     public Login(){
-
+        ui();
     }
     private void ui(){
-        JFrame loginjFrame = new JFrame();
-        loginjFrame.setSize(1050,800);
-        loginjFrame.setTitle("银行 - 登录界面");
-        loginjFrame.setAlwaysOnTop(true);
+        this.setSize(1050,800);
+        this.setTitle("银行 - 登录界面");
+        this.setAlwaysOnTop(true);
         //界面居中
-        loginjFrame.setLocationRelativeTo(null);
+        this.setLocationRelativeTo(null);
         //设置界面关闭模式
-        loginjFrame.setDefaultCloseOperation(3);
+        this.setDefaultCloseOperation(3);
         menu();
 
-        loginjFrame.setVisible(true);
+        this.setVisible(true);
     }
     private void menu(){
-        
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu menujMenu = new JMenu("  菜单  ");
+        JMenu functionjMenu = new JMenu("  功能  ");
+        JMenu exitjMenu = new JMenu("  退出  ");
+
+        JMenuItem loginjMenuItem = new JMenuItem("  登录  ");
+        JMenuItem enrolljMenuItem = new JMenuItem("  注册  ");
+
+        JMenuItem findjMenuItem = new JMenuItem("  查找  ");
+        JMenuItem revisejMenuItem = new JMenuItem("  修改  ");
+        JMenuItem deletejMenuItem = new JMenuItem("  删除  ");
+
+        menujMenu.add(loginjMenuItem);
+        menujMenu.add(enrolljMenuItem);
+
+        functionjMenu.add(findjMenuItem);
+        functionjMenu.add(revisejMenuItem);
+        functionjMenu.add(deletejMenuItem);
+
+
+        jMenuBar.add(menujMenu);
+        jMenuBar.add(functionjMenu);
+        jMenuBar.add(exitjMenu);
+        this.setJMenuBar(jMenuBar);
+
     }
 }
