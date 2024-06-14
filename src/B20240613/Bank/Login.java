3ff3e4bbe2883306a78824c3a11bd673cb3163b1
好喜欢安静啊 <3345973813@qq.com>
2024-06-14 15:45:14 +0800

    银行 - 登录 -- 设置登录界面居中

--- src/B20240613/Bank/Login.java
+++ src/B20240613/Bank/Login.java
@@ -7,6 +7,9 @@ public class Login {
         JFrame loginjFrame = new JFrame();
         loginjFrame.setSize(1050,800);
         loginjFrame.setTitle("银行 - 登录界面");
+        loginjFrame.setAlwaysOnTop(true);
+        //界面居中
+        loginjFrame.setLocationRelativeTo(null);
 
         loginjFrame.setVisible(true);
     }
