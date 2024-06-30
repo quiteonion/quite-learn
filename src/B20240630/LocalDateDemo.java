3a397981179ced4a912570e8232a5175cbb77f8c
好喜欢安静啊 <3345973813@qq.com>
2024-06-30 18:08:45 +0800

    设置时间 - LocalDate/LocalTime/LocalDateTime -- 静态方法 getXxx() --- 单一获得对应内容(getYear 就是获取年)

--- src/B20240630/LocalDateDemo.java
+++ src/B20240630/LocalDateDemo.java
@@ -7,12 +7,24 @@ import java.time.LocalTime;
 public class LocalDateDemo {
     public static void main(String[] args) {
 //        now();
-        of();
-
+//        of();
+        getXxx();
 
     }
-    public static void now(){
-        LocalDate localDate =LocalDate.now();
+
+    public static void getXxx() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("今天是今年的第几 天   " + localDateTime.getDayOfYear());
+        System.out.println("今天是今年的第几 月   " + localDateTime.getDayOfMonth());
+        System.out.println("今天是今年的第几 周   " + localDateTime.getDayOfWeek());
+        System.out.println("今年是 " + localDateTime.getYear());
+        System.out.println("获取今天的月数 " + localDateTime.getMonthValue());
+        System.out.println("这个月是 " + localDateTime.getMonth());
+        System.out.println("现在是几点 " + localDateTime.getHour());
+    }
+
+    public static void now() {
+        LocalDate localDate = LocalDate.now();
         System.out.println(localDate);
 
         LocalTime localTime = LocalTime.now();
@@ -21,14 +33,15 @@ public class LocalDateDemo {
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println(localDateTime);
     }
-    public static void of(){
-        LocalDate localDate =LocalDate.of(1996,2,5);
+
+    public static void of() {
+        LocalDate localDate = LocalDate.of(1996, 2, 5);
         System.out.println(localDate);
 
-        LocalTime localTime = LocalTime.of(17,47,26,46);
+        LocalTime localTime = LocalTime.of(17, 47, 26, 46);
         System.out.println(localTime);
 
-        LocalDateTime localDateTime = LocalDateTime.of(1996,2,5,17,47,26,46);
+        LocalDateTime localDateTime = LocalDateTime.of(1996, 2, 5, 17, 47, 26, 46);
         System.out.println(localDateTime);
     }
 }
