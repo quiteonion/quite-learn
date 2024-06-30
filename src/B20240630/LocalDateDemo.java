fa4f2737013c11eeeb34fa90497e3a313322f9dd
好喜欢安静啊 <3345973813@qq.com>
2024-06-30 17:39:03 +0800

    显示时间 - LocalDate -- 静态方法 now() 显示当前时间 --- 格式为：2024-06-30

---
+++ src/B20240630/LocalDateDemo.java
@@ -0,0 +1,11 @@
+package B20240630;
+
+import java.time.LocalDate;
+
+public class LocalDateDemo {
+    public static void main(String[] args) {
+
+        LocalDate localDate =LocalDate.now();
+        System.out.println(localDate);
+    }
+}
