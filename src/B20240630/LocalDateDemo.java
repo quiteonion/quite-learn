160cfe57783872bbf950fe434636953146ae05cf
好喜欢安静啊 <3345973813@qq.com>
2024-06-30 17:49:14 +0800

    设置时间 - LocalDate/LocalTime/LocalDateTime -- 静态方法 of() --- 设置时间

--- src/B20240630/LocalDateDemo.java
+++ src/B20240630/LocalDateDemo.java
@@ -6,7 +6,12 @@ import java.time.LocalTime;
 
 public class LocalDateDemo {
     public static void main(String[] args) {
+//        now();
+        of();
 
+
+    }
+    public static void now(){
         LocalDate localDate =LocalDate.now();
         System.out.println(localDate);
 
@@ -14,6 +19,16 @@ public class LocalDateDemo {
         System.out.println(localTime);
 
         LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+    }
+    public static void of(){
+        LocalDate localDate =LocalDate.of(1996,2,5);
+        System.out.println(localDate);
+
+        LocalTime localTime = LocalTime.of(17,47,26,46);
+        System.out.println(localTime);
+
+        LocalDateTime localDateTime = LocalDateTime.of(1996,2,5,17,47,26,46);
         System.out.println(localDateTime);
     }
 }
