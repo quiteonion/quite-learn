588c204c735f055285f91fd8000e1be5cf7acd7a
好喜欢安静啊 <3345973813@qq.com>
2024-07-06 21:12:01 +0800

    equals

---
+++ src/B20240706/Main.java
@@ -0,0 +1,9 @@
+package B20240706;
+
+public class Main {
+    public static void main(String[] args) {
+        String n = "qqq";
+        "qwe".equals(n);
+        System.out.println(n.compareTo("qwe"));
+    }
+}
