349e342676a7d0a2dc7d9dee6a91449ff9f15dd7
好喜欢安静啊 <3345973813@qq.com>
2024-07-09 18:38:48 +0800

    爬虫 - 局内爬虫 -- Pattern...
    Pattern 表示正则表达式

--- src/B20240709/PatternAndMatcher.java
+++ src/B20240709/PatternAndMatcher.java
@@ -1,7 +1,9 @@
 package B20240709;
 
+import java.util.regex.Pattern;
+
 public class PatternAndMatcher {
     public static void main(String[] args) {
-        
+        Pattern pattern = Pattern.compile("");
     }
 }
