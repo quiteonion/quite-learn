0da78a2490014ebc6473e4468fd270b0e5f28acd
好喜欢安静啊 <3345973813@qq.com>
2024-07-09 18:49:10 +0800

    爬虫 - 局内爬虫 -- Matcher...
    Matcher 中有两个方法
    find() ：从字符串第一个索引开始查找是否有与正则表达式匹配的，如果有匹配的记录为[左索引, 右索引+1]
    group() ：把当前匹配的获取出来 底层调用 subString 方法

--- src/B20240709/PatternAndMatcher.java
+++ src/B20240709/PatternAndMatcher.java
@@ -1,10 +1,19 @@
 package B20240709;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PatternAndMatcher {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile(".*");
-
+        Matcher matcher = pattern.matcher("21414562");
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+        /**
+         * Matcher 中有两个方法
+         * find() ：从字符串第一个索引开始查找是否有与正则表达式匹配的，如果有匹配的记录为[左索引, 右索引+1]
+         * group() ：把当前匹配的获取出来 底层调用 subString 方法（包左不包右）
+         */
     }
 }
