e8ff76120a9fda8be24e5730ae46b59da7494b1c
好喜欢安静啊 <3345973813@qq.com>
2024-07-09 18:39:19 +0800

    爬虫 - 局内爬虫 -- Matcher...
    Matcher 表示匹配器

--- src/B20240709/PatternAndMatcher.java
+++ src/B20240709/PatternAndMatcher.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class PatternAndMatcher {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("");
+        Pattern pattern = Pattern.compile(".*");
+
     }
 }
