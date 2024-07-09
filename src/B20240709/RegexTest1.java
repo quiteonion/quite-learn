acbeea2dfdfc817b3983ea9314c82bdaebdec638
好喜欢安静啊 <3345973813@qq.com>
2024-07-09 18:18:18 +0800

    正则表达式 - https://regexlearn.com/zh-cn/learn

---
+++ src/B20240709/RegexTest1.java
@@ -0,0 +1,9 @@
+package B20240709;
+
+public class RegexTest1 {
+    public static void main(String[] args) {
+        String m ="214241516 fafewaf.1y2~231124scac-80-90467";
+        String regex = ".*";
+        System.out.println(m.matches(regex));
+    }
+}
