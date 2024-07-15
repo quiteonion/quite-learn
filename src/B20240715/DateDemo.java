eafee65a08ca9cefe662fbda55dec8842dda2361
好喜欢安静啊 <3345973813@qq.com>
2024-07-15 15:06:36 +0800

    Date - Date about -- 空参...
    传入当前时间

--- src/B20240715/DateDemo.java
+++ src/B20240715/DateDemo.java
@@ -1,8 +1,11 @@
 package B20240715;
 
+import java.util.Date;
+
 public class DateDemo {
     public static void main(String[] args) {
         //关于时间
-
+        Date date = new Date();
+        System.out.println(date);
     }
 }
