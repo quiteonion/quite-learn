b0de26d5c6ffcfa4d3cc0496d229a4067656ae26
好喜欢安静啊 <3345973813@qq.com>
2024-07-16 16:03:22 +0800

    Calendar “日历” - about -- 抽象类，只能使用 getInstance 创建类，默认使用当前时间

---
+++ src/B20240716/CalendarDemo.java
@@ -0,0 +1,10 @@
+package B20240716;
+
+import java.util.Calendar;
+
+public class CalendarDemo {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        System.out.println(c);
+    }
+}
