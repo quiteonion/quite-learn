4be647d1c34cd69958221a77e2da8a55a44a9d58
好喜欢安静啊 <3345973813@qq.com>
2024-07-16 15:24:06 +0800

    SimpleDateFormat - about...
    格式化：把时间模式改变为某种格式
    解析： 把字符转为某种格式的时间

---
+++ src/B20240716/SimpleDateFormatDemo.java
@@ -0,0 +1,11 @@
+package B20240716;
+
+public class SimpleDateFormatDemo {
+    public static void main(String[] args) {
+        /**
+         * about SimpleFateFormat
+         * 格式化：把时间模式改变为某种格式
+         * 解析： 把字符转为某种格式的时间
+         */
+    }
+}
