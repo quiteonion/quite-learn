81e70e9624350a716dac206e0d07fb7ee1f7d257
好喜欢安静啊 <3345973813@qq.com>
2024-07-16 15:36:20 +0800

    SimpleDateFormat - format...
    按照格式化获取时间

--- src/B20240716/SimpleDateFormatDemo.java
+++ src/B20240716/SimpleDateFormatDemo.java
@@ -1,11 +1,18 @@
 package B20240716;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SimpleDateFormatDemo {
     public static void main(String[] args) {
         /**
          * about SimpleFateFormat
-         * 格式化：把时间模式改变为某种格式
-         * 解析： 把字符转为某种格式的时间
+         * 格式化：把时间模式改变为某种格式  format
+         * 解析： 把字符转为某种格式的时间  parse
          */
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 时：hh 分：mm 秒：ss  星期: EE");
+        Date date = new Date(0L);
+        String format = simpleDateFormat.format(date);
+        System.out.println(format);
     }
 }
