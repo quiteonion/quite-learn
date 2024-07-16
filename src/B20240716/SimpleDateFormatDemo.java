45c8a96bee7a37f9db19f4bba10c022d4c43722c
好喜欢安静啊 <3345973813@qq.com>
2024-07-16 15:38:42 +0800

    SimpleDateFormat - parse...
    按照格式化解析时间

--- src/B20240716/SimpleDateFormatDemo.java
+++ src/B20240716/SimpleDateFormatDemo.java
@@ -1,18 +1,24 @@
 package B20240716;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SimpleDateFormatDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         /**
          * about SimpleFateFormat
          * 格式化：把时间模式改变为某种格式  format
          * 解析： 把字符转为某种格式的时间  parse
          */
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 时：hh 分：mm 秒：ss  星期: EE");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         Date date = new Date(0L);
         String format = simpleDateFormat.format(date);
         System.out.println(format);
+
+        String str = "2023-06-09 16:00:00";
+        Date parse = simpleDateFormat.parse(str);
+        System.out.println(parse);
+        System.out.println(parse.getTime());
     }
 }
