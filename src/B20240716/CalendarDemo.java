a4a89d7b6b05e03180a87800c9942acd8fb4998a
好喜欢安静啊 <3345973813@qq.com>
2024-07-16 16:06:28 +0800

    Calendar “日历” - 细节 -- 在月份中是 0-11 所以显示的月份加一才是正确的

--- src/B20240716/CalendarDemo.java
+++ src/B20240716/CalendarDemo.java
@@ -1,10 +1,14 @@
 package B20240716;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarDemo {
     public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
+
+        Date d = new Date(0L);
+        c.setTime(d);
         System.out.println(c);
     }
 }
