1320199655ef5dfa220bcf77f6342d48f691312f
好喜欢安静啊 <3345973813@qq.com>
2024-07-17 16:32:33 +0800

    练习2 - 模拟 Integer.valueOf() -- 优化

--- src/B20240718/Test2.java
+++ src/B20240718/Test2.java
@@ -19,32 +19,34 @@ public class Test2 {
             return 0;
         }
         for (int i = 0; i < number.length(); i++) {
-            char c = number.charAt(i);
-            boolean o = c >= '0' && c <= '9';
-            if (!o) {
-                return 0;
-            }
-            if (c == '0') {
-                mun = mun * 10;
-            } else if (c == '1') {
-                mun = mun * 10 + 1;
-            } else if (c == '2') {
-                mun = mun * 10 + 2;
-            } else if (c == '3') {
-                mun = mun * 10 + 3;
-            } else if (c == '4') {
-                mun = mun * 10 + 4;
-            } else if (c == '5') {
-                mun = mun * 10 + 5;
-            } else if (c == '6') {
-                mun = mun * 10 + 6;
-            } else if (c == '7') {
-                mun = mun * 10 + 7;
-            } else if (c == '8') {
-                mun = mun * 10 + 8;
-            } else {
-                mun = mun * 10 + 9;
-            }
+            int n = number.charAt(i) - '0';
+            mun = mun * 10 + n;
+//            char c = number.charAt(i);
+//            boolean o = c >= '0' && c <= '9';
+//            if (!o) {
+//                return 0;
+//            }
+//            if (c == '0') {
+//                mun = mun * 10;
+//            } else if (c == '1') {
+//                mun = mun * 10 + 1;
+//            } else if (c == '2') {
+//                mun = mun * 10 + 2;
+//            } else if (c == '3') {
+//                mun = mun * 10 + 3;
+//            } else if (c == '4') {
+//                mun = mun * 10 + 4;
+//            } else if (c == '5') {
+//                mun = mun * 10 + 5;
+//            } else if (c == '6') {
+//                mun = mun * 10 + 6;
+//            } else if (c == '7') {
+//                mun = mun * 10 + 7;
+//            } else if (c == '8') {
+//                mun = mun * 10 + 8;
+//            } else {
+//                mun = mun * 10 + 9;
+//            }
 
         }
         return mun;
