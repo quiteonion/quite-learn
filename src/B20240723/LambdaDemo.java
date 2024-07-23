43858d2b15abeaeeef4570059ba6827fb057efb9
好喜欢安静啊 <3345973813@qq.com>
2024-07-23 14:09:01 +0800

    Lambda -  函数式编程，将复杂的匿名类简化

--- src/B20240723/LambdaDemo.java
+++ src/B20240723/LambdaDemo.java
@@ -6,11 +6,9 @@ import java.util.Comparator;
 public class LambdaDemo {
     public static void main(String[] args) {
         Integer[] arr = {2, 4, 1, 3, 8, 5, 9, 6, 7, 0};
-        Arrays.sort(arr, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 - o2;
+        Arrays.sort(arr, (Integer o1, Integer o2) -> {
+            return o1 - o2;
             }
-        });
+        );
     }
 }
