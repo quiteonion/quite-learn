e998313971b06896c58734c6460908e3036820ee
好喜欢安静啊 <3345973813@qq.com>
2024-07-30 15:13:24 +0800

    枚举 - 使用 Class 类写出枚举的格式

---
+++ src/B20240730/Enum_demo/day_class/DayTest.java
@@ -0,0 +1,8 @@
+package B20240730.Enum_demo.day_class;
+
+public class DayTest {
+    public static void main(String[] args) {
+        DayClass friday = DayClass.FRIDAY;
+        System.out.println(friday);
+    }
+}
