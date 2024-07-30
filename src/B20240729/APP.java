a82a27cb3de37b49c3c9f58273ffb498cccafb40
好喜欢安静啊 <3345973813@qq.com>
2024-07-30 10:34:15 +0800

    编程思想延续 - 黑盒子思想 -- 输出模块 - 补充黑盒子

--- src/B20240729/APP.java
+++ src/B20240729/APP.java
@@ -19,9 +19,11 @@ public class APP {
         System.out.println("准备存入..." + url);
         Ropository ropository = new XMFishRopository();
         List<URLContent> ls = ropository.getHtml(list);
-        System.out.println("存入完成..." + url);        
+        System.out.println("存入完成..." + url);
         //输出
+        System.out.println("准备输出..." + url);
         Tificator tificator = new XMFishTificator();
         tificator.up(ls);
+        System.out.println("结束输出..." + url);
     }
 }
--- src/B20240729/XMFishTificator.java
+++ src/B20240729/XMFishTificator.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class XMFishTificator extends Tificator{
     @Override
     public void up(List<URLContent> list) {
-        System.out.println("打印输出");
+        for (URLContent content : list) {
+            System.out.println(content.toString());
+        }
     }
 }
