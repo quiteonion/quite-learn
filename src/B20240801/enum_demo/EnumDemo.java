cd2447a44e1966904869f3215060b07ebf5ce0d3
好喜欢安静啊 <3345973813@qq.com>
2024-08-01 14:23:30 +0800

    枚举 - 可以在枚举的每一个属性内实现接口

--- src/B20240801/enum_demo/EnumDemo.java
+++ src/B20240801/enum_demo/EnumDemo.java
@@ -1,15 +1,47 @@
 package B20240801.enum_demo;
 
 public enum EnumDemo implements ForEnumDemo{
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
-    @Override
-    public void desc() {
-        System.out.println("qwert");
-    }
+    MONDAY{
+        @Override
+        public void desc() {
+            System.out.println("1");
+        }
+    },
+    TUESDAY{
+        @Override
+        public void desc() {
+            System.out.println("2");
+        }
+    },
+    WEDNESDAY{
+        @Override
+        public void desc() {
+            System.out.println("3");
+        }
+    },
+    THURSDAY{
+        @Override
+        public void desc() {
+            System.out.println("4");
+        }
+    },
+    FRIDAY{
+        @Override
+        public void desc() {
+            System.out.println("5");
+        }
+    },
+    SATURDAY{
+        @Override
+        public void desc() {
+            System.out.println("6");
+        }
+    },
+    SUNDAY{
+        @Override
+        public void desc() {
+            System.out.println("7");
+        }
+    };
+
 }
