0f94deb0f88f0139af08ab2a39bc3cc3b47dfe8f
好喜欢安静啊 <3345973813@qq.com>
2024-08-02 11:19:58 +0800

    Thread - 将匿名内部类改为 Lambda

--- src/B20240802/ThreadTest2.java
+++ src/B20240802/ThreadTest2.java
@@ -6,17 +6,15 @@ public class ThreadTest2 {
         threadDemo1.start();
         ThreadDemo2 threadDemo2 = new ThreadDemo2();
         new Thread(threadDemo2).start();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    System.out.println("This is " + Thread.currentThread().getName() + " - " + i);
-                }
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("This is " + Thread.currentThread().getName() + " - " + i);
             }
         }).start();
     }
 }
-class ThreadDemo1 extends Thread{
+
+class ThreadDemo1 extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
@@ -25,7 +23,7 @@ class ThreadDemo1 extends Thread{
     }
 }
 
-class ThreadDemo2 implements Runnable{
+class ThreadDemo2 implements Runnable {
 
     @Override
     public void run() {
