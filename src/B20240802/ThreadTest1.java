d7811e28bf8f58bdb4cb2de4e7d20f8ab7b0f72a
好喜欢安静啊 <3345973813@qq.com>
2024-08-02 10:29:58 +0800

    Thread - 有参构造...
    形参一： 要增加的线程
    形参二： 该线程的名称

--- src/B20240802/ThreadTest1.java
+++ src/B20240802/ThreadTest1.java
@@ -6,21 +6,25 @@ public class ThreadTest1 {
         thread1.start();
         Thread2 thread2 = new Thread2();
         thread2.start();
+        Thread thread = new Thread(new Thread1(), "Thread3");
+        thread.start();
     }
 }
-class Thread1 extends Thread{
+
+class Thread1 extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println("This is Thread1 - " + i);
+            System.out.println("This is " + Thread.currentThread().getName() + " - " + i);
         }
     }
 }
-class Thread2 extends Thread{
+
+class Thread2 extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println("This is Thread2 + " + i);
+            System.out.println("This is " + Thread.currentThread().getName() + " + " + i);
         }
     }
 }
