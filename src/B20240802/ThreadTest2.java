0b83209281fff7c18da375393c781b523ddff8b4
好喜欢安静啊 <3345973813@qq.com>
2024-08-02 11:19:10 +0800

    Thread - 使用匿名类 Runnable 的方式创建并重写 run 方法

--- src/B20240802/ThreadTest2.java
+++ src/B20240802/ThreadTest2.java
@@ -6,6 +6,14 @@ public class ThreadTest2 {
         threadDemo1.start();
         ThreadDemo2 threadDemo2 = new ThreadDemo2();
         new Thread(threadDemo2).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("This is " + Thread.currentThread().getName() + " - " + i);
+                }
+            }
+        }).start();
     }
 }
 class ThreadDemo1 extends Thread{
