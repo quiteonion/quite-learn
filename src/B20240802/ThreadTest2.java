c4abcfeb957d0d306b5a8a044cf273c68e21d7a3
好喜欢安静啊 <3345973813@qq.com>
2024-08-02 11:16:59 +0800

    Thread - 使用实现接口 Runnable 的方式创建并重写 run 方法

--- src/B20240802/ThreadTest2.java
+++ src/B20240802/ThreadTest2.java
@@ -4,6 +4,8 @@ public class ThreadTest2 {
     public static void main(String[] args) {
         ThreadDemo1 threadDemo1 = new ThreadDemo1();
         threadDemo1.start();
+        ThreadDemo2 threadDemo2 = new ThreadDemo2();
+        new Thread(threadDemo2).start();
     }
 }
 class ThreadDemo1 extends Thread{
@@ -15,3 +17,12 @@ class ThreadDemo1 extends Thread{
     }
 }
 
+class ThreadDemo2 implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("This is " + Thread.currentThread().getName() + " - " + i);
+        }
+    }
+}
