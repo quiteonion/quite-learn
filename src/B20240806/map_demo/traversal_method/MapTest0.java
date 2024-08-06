29efce2aba1a58495f1550a91ae0a569dd435bdd
Serene_Dream <3345973813@qq.com>
2024-08-06 10:31:38 +0800

    map 的遍历方式 - 使用 keySet 获取 key 数组后进行遍历

---
+++ src/B20240806/map_demo/traversal_method/MapTest0.java
@@ -0,0 +1,25 @@
+package B20240806.map_demo.traversal_method;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapTest0 {
+    public static void main(String[] args) {
+        /**
+         * map 的遍历方式
+         * 1、使用 keySet 获取 key 数组后进行遍历
+         */
+        Map<String, String> m = new HashMap<>();
+        m.put("aaa","aaa");
+        m.put("bbb","bbb");
+        m.put("ccc","ccc");
+        m.put("ddd","ddd");
+
+        Set<String> set = m.keySet();
+        for (String s : set) {
+            System.out.println(s);
+            System.out.println(m.get(s));
+        }
+    }
+}
