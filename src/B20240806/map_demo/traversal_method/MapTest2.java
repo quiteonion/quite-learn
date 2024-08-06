0a2b38585b82941be38d73ccbb50179860914b6c
Serene_Dream <3345973813@qq.com>
2024-08-06 12:52:14 +0800

    map 的遍历方式 - 使用 forEach 重写 BiConsumer 进行遍历

---
+++ src/B20240806/map_demo/traversal_method/MapTest2.java
@@ -0,0 +1,27 @@
+package B20240806.map_demo.traversal_method;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class MapTest2 {
+    public static void main(String[] args) {
+        /**
+         * map 的遍历方式
+         * 使用 forEach 重写 BiConsumer 进行遍历
+         */
+        Map<String, String> m = new HashMap<>();
+        m.put("aaa","aaa");
+        m.put("bbb","bbb");
+        m.put("ccc","ccc");
+        m.put("ddd","ddd");
+
+        m.forEach(new BiConsumer<String, String>() {
+            @Override
+            public void accept(String s, String s2) {
+                System.out.println(s);
+                System.out.println(s2);
+            }
+        });
+    }
+}
