523bd737ac26e0a5db23dfbb175944b1ee9b07bf
Serene_Dream <3345973813@qq.com>
2024-08-11 13:33:26 +0800

    呆了个呆

--- src/io/github/quiteonion/test/BrandUtil.java
+++ src/io/github/quiteonion/test/BrandUtil.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class BrandUtil {
     public static Random r = new Random();
     public static String[] brandNames = {
-            "假面骑士", "地狱火", "带土", "羊", "鸡哥"
+            "假面骑士", "地狱火", "带土", "羊", "鸡哥","一豪十分开心","什么东东","巧V嚣张跋扈","模糊巧","精神力MAX","老师豪","连麦宇将军"
     };
 
     public static String getBrand() {
