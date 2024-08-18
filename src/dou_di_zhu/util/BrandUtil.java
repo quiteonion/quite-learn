e0c5d26da5bc5cfc023ff5bca685d678b3f57962
Serene_Dream <3345973813@qq.com>
2024-08-18 12:14:21 +0800

    斗地主 - 在工具类中对对应牌的大小进行定义

--- src/dou_di_zhu/util/BrandUtil.java
+++ src/dou_di_zhu/util/BrandUtil.java
@@ -2,10 +2,7 @@ package dou_di_zhu.util;
 
 import dou_di_zhu.Brand;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 工具类，用来打乱牌和整理每个人手上的牌
@@ -20,26 +17,29 @@ public class BrandUtil {
         for (int j = 0; j < numbers.length; j++) {
             for (int k = 0; k < colors.length; k++) {
                 Brand brand = new Brand(colors[k], numbers[j]);
+                brand.setInteger(i);
                 brands[i++] = brand;
             }
         }
-        brands[52] = new Brand(" ", "大鬼");
-        brands[53] = new Brand(" ", "小鬼");
+        brands[52] = new Brand(" ", "小鬼");
+        brands[52].setInteger(52);
+        brands[53] = new Brand(" ", "大鬼");
+        brands[53].setInteger(53);
         return brands;
     }
-    public static Brand[] shuffle(){
-        if (brands[0] == null ){
+
+    public static Brand[] shuffle() {
+        if (brands[0] == null) {
             System.out.println("请先创建牌组才能进行打乱");
             return null;
         }
         List<Brand> brandList = new ArrayList<>();
-        for (int i = 0; i < brands.length; i++) {
-            brandList.add(brands[i]);
-        }
+        brandList.addAll(Arrays.asList(brands));
         Collections.shuffle(brandList);
         for (int i = 0; i < brands.length; i++) {
             brands[i] = brandList.get(i);
         }
         return brands;
     }
+
 }
