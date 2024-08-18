9cc4f78ecc9b0d71bb2a28bd8adc31b58307c917
Serene_Dream <3345973813@qq.com>
2024-08-18 12:29:27 +0800

    斗地主 - 实践类中观察可行

--- src/dou_di_zhu/test/BrandTest.java
+++ src/dou_di_zhu/test/BrandTest.java
@@ -10,8 +10,16 @@ public class BrandTest {
     public static void main(String[] args) {
         Brand[] brands = BrandUtil.createBrands();
         System.out.println(Arrays.toString(brands));
+        System.out.println("===== 洗牌 =====");
         Brand[] shuffle = BrandUtil.shuffle();
         System.out.println(Arrays.toString(shuffle));
+        System.out.println("===== 分牌 =====");
+        List<Brand[]> deal = BrandUtil.deal(shuffle);
+        for (int i = 0; i < deal.size(); i++) {
+            Brand[] brands1 = deal.get(i);
+            System.out.println(Arrays.toString(brands1));
+        }
+        System.out.println("===== 理牌 =====");
         BrandUtil.sort(shuffle);
         System.out.println(Arrays.toString(shuffle));
     }
