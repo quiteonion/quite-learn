8ec5897843be71c75dc8819e16ce1a7c1155d279
Serene_Dream <3345973813@qq.com>
2024-08-18 12:14:36 +0800

    斗地主 - 实践类中观察可行

--- src/dou_di_zhu/test/BrandTest.java
+++ src/dou_di_zhu/test/BrandTest.java
@@ -4,12 +4,15 @@ import dou_di_zhu.Brand;
 import dou_di_zhu.util.BrandUtil;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class BrandTest {
     public static void main(String[] args) {
         Brand[] brands = BrandUtil.createBrands();
         System.out.println(Arrays.toString(brands));
         Brand[] shuffle = BrandUtil.shuffle();
+        System.out.println(Arrays.toString(shuffle));
+        Arrays.sort(shuffle);
         System.out.println(Arrays.toString(shuffle));
     }
 }
