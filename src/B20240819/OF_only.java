ca3a7553f1bc37b63344ca35c572eb5d0867bb64
Serene_Dream <3345973813@qq.com>
2024-08-19 10:21:18 +0800

    不可变集合：使用 of 方法创建不可变集合

---
+++ src/B20240819/OF_only.java
@@ -0,0 +1,14 @@
+package B20240819;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class OF_only {
+    public static void main(String[] args) {
+        List<String> list = List.of("1","2","3","4","5");
+        Set<String> set = Set.of("1","2","3","4","5");
+        Map<String , String> map = Map.of();
+     }
+}
