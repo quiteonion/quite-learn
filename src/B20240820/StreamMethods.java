1a7392eaf66a78c1e935a65c435d32535cf1eedc
Serene_Dream <3345973813@qq.com>
2024-08-27 15:53:15 +0800

    stream 的中间方法 - distinct -- 元素去重，依赖于 hashcode() 和 equals()

--- src/B20240820/StreamMethods.java
+++ src/B20240820/StreamMethods.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 
 public class StreamMethods {
     public static void main(String[] args) {
-        String[] string = {"张吉惟", "林国瑞", "林玟书", "林雅南", "江奕云", "刘柏宏", "阮建安", "林子帆", "夏志豪", "吉茹定", "李中冰", "黄文隆"};
+        String[] string = {"张吉惟","张吉惟","张吉惟","张吉惟","张吉惟","张吉惟", "林国瑞", "林玟书", "林雅南", "江奕云", "刘柏宏", "阮建安", "林子帆", "夏志豪", "吉茹定", "李中冰", "黄文隆"};
         /**
          * filter
          * 过滤，相当于加入一个筛网，把自己不需要的过滤
@@ -35,5 +35,20 @@ public class StreamMethods {
         Stream<String> stream2 = Arrays.stream(string);
         Stream<String> skip = stream2.skip(3);
         skip.forEach(s -> System.out.println(s));
+
+        System.out.println("==== === ====");
+
+        /**
+         * distinct
+         * 元素去重
+         */
+
+        Stream<String> stream3 = Arrays.stream(string);
+        Stream<String> distinct = stream3.distinct();
+        distinct.forEach(s -> System.out.println(s));
+
+        System.out.println("==== === ====");
+
+
     }
 }
