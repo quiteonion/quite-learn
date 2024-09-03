3a75c5c1e910e270892faa70b4885c7e4cd2960a
Serene_Dream <3345973813@qq.com>
2024-09-03 18:13:25 +0800

    IO 流 - 利用 IO 流的读写功能

--- src/B20240903/MyIODemo1.java
+++ src/B20240903/MyIODemo1.java
@@ -2,17 +2,22 @@ package B20240903;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MyIODemo1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //IO 流的简称便是读取流，和输出流
         //我们可以通过 new 出一个 FileInputStream 来获取文件夹中对应的输入流  ↓
-        FileInputStream fis1 = new FileInputStream("src/B20240903/inputFile/inputTxt.txt");
+        FileInputStream fis1 = new FileInputStream("src/B20240903/inputFile/inputMovie.mp4");
         //当然，也可以通过 new 出一个 FileOutputStream 来写入想要写入的内容  ↓
         //这里会存在编译时报错，为了提醒你现在是否将文件名称填写正确了
-        FileOutputStream fos1 = new FileOutputStream("src/B20240903/outFile/outputTxt.txt");
+        FileOutputStream fos1 = new FileOutputStream("src/B20240903/outFile/inputMovie.mp4");
         /**
          * 在两个构造方法的形参中同时拥有第二个参数，这个参数的作用是 是否在上一次的基础上继续向下书写
          */
+        int l;
+        while ((l = fis1.read()) != -1){
+            fos1.write(l);
+        }
     }
 }
